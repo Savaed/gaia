a43f2b5d588b1d56ef7c91e756998b03fda9bf99
Krzysztof Gonet <36476874+Savaed@users.noreply.github.com>
2022-08-17 17:03:01 +0200

    Use single series in most time series functions

--- gaia/data/ops/time_series.py
+++ gaia/data/ops/time_series.py
@@ -1,7 +1,6 @@
 """Preprocessing functions for time series."""
 
-from collections import defaultdict
-from typing import Union
+from typing import Optional, Protocol, Union
 
 import numpy as np
 
@@ -19,7 +18,7 @@ def wrap_in_list(segments: _Segments) -> _Segments:
     return segments
 
 
-def split_arrays(time: _Segments, *series: _Segments, gap_with: float = 0.75) -> _MultiDataSegments:
+def split_arrays(time: _Segments, series: _Segments, gap_with: float = 0.75) -> _MultiDataSegments:
     """Split time series tables at gaps.
 
     Accepts either a single time segment and time series segment or a list of such segments.
@@ -28,7 +27,7 @@ def split_arrays(time: _Segments, *series: _Segments, gap_with: float = 0.75) ->
     ----------
     time : _Segments
         Time of observations. Single numpy array or a list of arrays
-    *series : _Segments
+    series : _Segments
         Time series associated with the `time` parameter. Single numpy array or a list of arrays
 
     gap_with : float, optional
@@ -50,26 +49,26 @@ def split_arrays(time: _Segments, *series: _Segments, gap_with: float = 0.75) ->
         raise ValueError(f"'gap_width' must be grater than zero, but got {gap_with=}'")
 
     time = wrap_in_list(time)
-    out_ts = defaultdict(lambda: [])
+    out_ts = []
     out_time = []
     split_indicies = [np.argwhere(np.diff(t) > gap_with).flatten() + 1 for t in time]
 
-    for time_segment, split_indx in zip(time, split_indicies):
+    for time_segment, series_segment, split_indx in zip(time, series, split_indicies):
         out_time.extend(np.array_split(time_segment, split_indx))
+        out_ts.extend(np.array_split(series_segment, split_indx))
 
-    for series_indx, time_series in enumerate(series):
-        time_series = wrap_in_list(time_series)
-
-        for split_indx, time_series_segment in zip(split_indicies, time_series):
-            out_ts[series_indx].extend(np.array_split(time_series_segment, split_indx))
-
-    return out_time, *out_ts.values()
+    return out_time, out_ts
 
 
 def phase_fold_time(
-    time: np.ndarray, epoch: float, period: float, sort: bool = False
-) -> np.ndarray:
-    """Create a phase-folded time vector.
+    time: np.ndarray,
+    *,
+    epoch: float,
+    period: float,
+    series: Optional[np.ndarray] = None,
+    sort: bool = False,
+) -> Union[np.ndarray, tuple[np.ndarray, np.ndarray]]:
+    """Create a phase-folded vector.
 
     Parameters
     ----------
@@ -79,14 +78,18 @@ def phase_fold_time(
         First transit occurrence. This value is mapped to zero
     period : float
         Period to fold over
+    series: np.ndarray
+        A sequence of time series features corresponding to the `time`
     sort : bool, optional
-        Whether to sort a folded time in ascending order, by default False
+        Whether to sort a folded values by `time` in ascending order, by default False
 
     Returns
     -------
-    np.ndarray
-        1D numpy array with time folded around a `period` with all values within
-        `[-period / 2, period / 2]`
+    Union[np.ndarray, tuple[np.ndarray, np.ndarray]]
+        1D numpy arrays with folded around a `period` with time values within
+        `[-period / 2, period / 2]` if only `time` is passed.
+        A tuple of two 1D numpy arrays with folded around a `period` with time values within
+        `[-period / 2, period / 2]` and series values folded in the same way
 
     Raises
     ------
@@ -97,17 +100,28 @@ def phase_fold_time(
         raise ValueError(f"'period' must be grater than zero, but got {period=}")
 
     half_period = period / 2
-    result = np.mod(time + (half_period - epoch), period)
-    result -= half_period
-    return np.sort(result) if sort else result
+    folded_time = np.mod(time + (half_period - epoch), period)
+    folded_time -= half_period
+    order = np.argsort(folded_time) if sort else np.arange(time.size)
+    return folded_time[order] if series is None else (folded_time[order], series[order])
+
+
+class EventRemovingWidthStrategy(Protocol):
+    def __call__(self, duration: float, period: float) -> float:
+        ...
+
+
+class AdjustedPaddingRemoving:
+    def __call__(self, duration: float, period: float) -> float:
+        return min(3 * duration, period)
 
 
 def remove_events(
     all_time: _Segments,
     events: list[PeriodicEvent],
-    width_factor: float = 1.0,
+    series: _Segments,
+    compute_removing_width: EventRemovingWidthStrategy,
     include_empty_segments: bool = True,
-    **series: _Segments,
 ) -> _MultiDataSegments:
     """Remove events from a time series.
 
@@ -132,33 +146,33 @@ def remove_events(
         The first element is `all_time` followed by `series` elements.
     """
     if not events:
-        return all_time, *series.values()
+        return all_time, series
 
     all_time = wrap_in_list(all_time)
-
+    series = wrap_in_list(series)
     out_time = []
-    out_series = defaultdict(lambda: [])
+    out_series = []
 
-    for segment_num, time_segment in enumerate(all_time):
+    for time_segment, series_segment in zip(all_time, series):
         mask = np.ones_like(time_segment)
+
         for event in events:
-            transit_dist = np.abs(phase_fold_time(time_segment, event.epoch, event.period))
-            mask = np.logical_and(mask, transit_dist > 0.5 * width_factor * event.duration)
+            transit_dist = np.abs(
+                phase_fold_time(time_segment, epoch=event.epoch, period=event.period)
+            )
+            removing_width = compute_removing_width(event.duration, event.period)
+            mask = np.logical_and(mask, transit_dist > 0.5 * removing_width)
 
         if include_empty_segments or mask.any():
             out_time.append(time_segment[mask])
+            out_series.append(series_segment[mask])
 
-            for series_ind, ts in enumerate(series.values()):
-                ts = wrap_in_list(ts)
-                series_masked_segment = ts[segment_num][mask]
-                out_series[series_ind].append(series_masked_segment)
-
-    return out_time, *out_series.values()
+    return out_time, out_series
 
 
 def interpolate_masked_spline(
-    all_time: _Segments, all_masked_time: _Segments, *all_masked_splines: _Segments
-) -> _MultiDataSegments:
+    all_time: _Segments, all_masked_time: _Segments, all_masked_splines: _Segments
+) -> list[np.ndarray]:
     """Linearly interpolate spline values across masked points.
 
     Accepts either a single time segment and time series segment or a list of such segments.
@@ -170,7 +184,7 @@ def interpolate_masked_spline(
     all_masked_time : _Segments
         A sequence of time values of observations with some values missing (masked). Single numpy
         array or a list of arrays
-    *all_masked_splines : tuple[_Segments]
+    all_masked_splines : tuple[_Segments]
         Masked spline values corresponding to `all_masked_time`. Each tuple element is a single
         numpy array or a list of arrays
 
@@ -179,20 +193,16 @@ def interpolate_masked_spline(
     _MultiDataSegments
         The tuple of the list of numpy arrays. The tuple is of the size of `all_masked_splines`
         size. Each tuple element is the list of masked splines with missing points linearly
-        interpolated. The first element is `all_time` followed by `series` elements.
+        interpolated.
     """
-    interp_splines = defaultdict(lambda: [])
-
     all_time = wrap_in_list(all_time)
     all_masked_time = wrap_in_list(all_masked_time)
+    interp_splines = []
 
-    for spline_indx, all_masked_spline in enumerate(all_masked_splines):
-        all_masked_spline = wrap_in_list(all_masked_spline)
-        for time, masked_time, masked_spline in zip(all_time, all_masked_time, all_masked_spline):
-            if masked_time.size:
-                interp_splines[spline_indx].append(np.interp(time, masked_time, masked_spline))
-            else:
-                interp_splines[spline_indx].append(np.array([np.nan] * len(time)))
+    for time, masked_time, masked_spline in zip(all_time, all_masked_time, all_masked_splines):
+        if masked_time.size:
+            interp_splines.append(np.interp(time, masked_time, masked_spline))
+        else:
+            interp_splines.append(np.array([np.nan] * len(time)))
 
-    out_splines = tuple(list(spline_values) for spline_values in interp_splines.values())
-    return out_splines
+    return interp_splines
