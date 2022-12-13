b624f237d47a3614adde47ab103d4bdca269396c
Krzysztof Gonet <36476874+Savaed@users.noreply.github.com>
2022-12-13 23:16:41 +0100

    Move download() from api.py to http.py

---
+++ gaia/http.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+
+import aiohttp
+
+
+@dataclass
+class ApiError(Exception):
+    """Raised when the HTTP request to the NASA API is incorrect."""
+
+    message: str
+    status: int
+    url: str | None
+
+    def __str__(self) -> str:  # pragma: no cover
+        return f"{self.status}: {self.url or ''} {self.message}"
+
+
+async def download(url: str, session: aiohttp.ClientSession) -> bytes:
+    """Download data from online resources via REST API.
+
+    Args:
+        url (str): The source URL
+        session (aiohttp.ClientSession): HTTP session
+
+    Raises:
+        ApiError: Underlying OS error or malformed request (e.g. HTTP 404 Not Found)
+
+    Returns:
+        bytes: Response data
+    """
+    try:
+        async with session.get(url, raise_for_status=True) as resp:
+            return await resp.read()  # type: ignore
+    except aiohttp.ClientOSError as ex:
+        raise ApiError(message=ex.strerror, status=ex.errno, url=url)
+    except aiohttp.ClientResponseError as ex:
+        raise ApiError(message=ex.message, status=ex.status, url=url)
