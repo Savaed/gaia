707d97f9b83b37cf1d8c0a5330124c3b78dd095f
Krzysztof Gonet <36476874+Savaed@users.noreply.github.com>
2023-01-02 17:43:55 +0100

    Add missing check_kepid() docstring

--- gaia/utils.py
+++ gaia/utils.py
@@ -1,3 +1,11 @@
 def check_kepid(kepid: int) -> None:
+    """Check the validity of the passed `kepid`.
+
+    Args:
+        kepid (int): Target (KOI/KIC) identifier
+
+    Raises:
+        ValueError: `kepid` is outside [1, 999 999 999]
+    """
     if not 0 < kepid < 1_000_000_000:
         raise ValueError(f"'kepid' must be in range 1 to 999 999 999 inclusive, but got {kepid=}")
