e0b45438fff209ed7c5d3b240fe1bd53756f58af
Krzysztof Gonet <36476874+Savaed@users.noreply.github.com>
2023-01-26 12:26:26 +0100

    Add retry decorator

--- gaia/utils.py
+++ gaia/utils.py
@@ -1,11 +1,75 @@
+import asyncio
+import functools
+import random
+from collections.abc import Awaitable, Callable
+from typing import ParamSpec, Type, TypeAlias, TypeVar
+
+import structlog
+
+
+log = structlog.stdlib.get_logger()
+
+
 def check_kepid(kepid: int) -> None:
     """Check the validity of the passed `kepid`.
 
     Args:
-        kepid (int): Target (KOI/KIC) identifier
+        kepid (int): Target identifier (KOI/KIC)
 
     Raises:
         ValueError: `kepid` is outside [1, 999 999 999]
     """
     if not 0 < kepid < 1_000_000_000:
         raise ValueError(f"'kepid' must be in range 1 to 999 999 999 inclusive, but got {kepid=}")
+
+
+Errors: TypeAlias = Type[Exception] | tuple[Type[Exception], ...]
+P = ParamSpec("P")
+R = TypeVar("R")
+
+
+def retry(
+    retries: int = 5,
+    max_seconds: int = 64,
+    on: Errors | None = None,
+) -> Callable[[Callable[P, Awaitable[R]]], Callable[P, Awaitable[R]]]:
+    """A decorator to retry an asynchronous function with exponential backoff.
+
+    Backoff is based on https://cloud.google.com/iot/docs/how-tos/exponential-backoff
+
+    Args:
+        retries (int, optional): How many retries. Must be at least 1. Defaults to 5.
+        max_seconds (int, optional): Upper limit for backoff. Defaults to 64.
+        on (Errors | None, optional): Error(s) on which retry. If None, retry each `Exception`
+            error. Defaults to None.
+
+    Raises:
+        ValueError: `retries` less than 1
+    """
+    if retries < 1:
+        raise ValueError(f"'retries' must be at least 1, but got {retries=}")
+
+    errors = on or Exception
+
+    def wrapper(fn: Callable[P, Awaitable[R]]) -> Callable[P, Awaitable[R]]:
+        @functools.wraps(fn)
+        async def wrapped(*args: P.args, **kwargs: P.kwargs) -> R:
+            n = 0
+            while True:
+                try:
+                    return await fn(*args, **kwargs)
+                except errors as ex:
+                    n += 1
+                    log.warning(ex)
+                    backoff = min((n**2) + random.random(), max_seconds)
+
+                    if n > retries:
+                        log.error("Retries limit reached")
+                        raise
+
+                    log.warning(f"Retrying {n}/{retries}", backoff_seconds=round(backoff, 2))
+                    await asyncio.sleep(backoff)
+
+        return wrapped
+
+    return wrapper
--- tests/utils_test.py
+++ tests/utils_test.py
@@ -1,13 +1,43 @@
+from unittest.mock import AsyncMock
+
 import pytest
 
-from gaia.utils import check_kepid
+from gaia.utils import check_kepid, retry
 
 
 @pytest.mark.parametrize("kepid", [-1, 0, 1_000_000_000])
 def test_check_kepid__invalid_input(kepid):
+    """Test that ValueError is raised when kepid is outside [1, 999 999 999]."""
     with pytest.raises(ValueError):
         check_kepid(kepid)
 
 
 def test_check_kepid__valid_input():
+    """Test that no error is raised when kepid is inside [1, 999 999 999]."""
     check_kepid(123)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "n,results",
+    [(1, [Exception(), "ok"]), (2, [Exception(), Exception(), "ok"])],
+    ids=[1, 2],
+)
+async def test_retry__retrying_specified_times(n, results, mocker):
+    """Test check whether retries specified number of times."""
+    mocker.patch("gaia.utils.asyncio.sleep")
+    foo = AsyncMock(side_effect=results)
+    deco = retry(n)(foo)
+    await deco()
+    assert foo.await_count == n + 1
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("retries", [-1, 0])
+async def test_retry__retries_number_less_than_1(retries):
+    """Test that ValueError is raised when specified retries number is less than 1."""
+    with pytest.raises(ValueError, match="'retries' must be at least 1"):
+
+        @retry(retries)
+        async def _():
+            ...
