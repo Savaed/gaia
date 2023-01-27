f40512ef87ef896833f55a1305dd6cd92ebec58d
Krzysztof Gonet <36476874+Savaed@users.noreply.github.com>
2023-01-27 23:54:56 +0100

    Simplify downloaders.py and its tests code

--- gaia/downloaders.py
+++ gaia/downloaders.py
@@ -8,8 +8,11 @@ For more information on what you can download, see:
    https://archive.stsci.edu/missions-and-data/kepler/kepler-bulk-downloads - MAST
 """
 
+import asyncio
+import concurrent.futures
+import functools
 import re
-from collections.abc import Iterable
+from collections.abc import Iterable, Iterator
 from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Protocol, TypeAlias
@@ -17,20 +20,21 @@ from typing import Protocol, TypeAlias
 import aiohttp
 import structlog
 
+from gaia.enums import Cadence
 from gaia.http import ApiError, download
 from gaia.io import Saver
+from gaia.quarters import get_quarter_prefixes
+from gaia.utils import retry
 
 
 log = structlog.stdlib.get_logger()
-
-DownloadResult: TypeAlias = tuple[str, bytes]
 
 
 @dataclass
 class TableRequest:
     name: str
     query: str | None = None
-    columns: set[str] = field(default_factory=set)
+    columns: list[str] = field(default_factory=list)
 
 
 class Downloader(Protocol):
@@ -41,21 +45,46 @@ class Downloader(Protocol):
         ...
 
 
+class _MissingFileError(Exception):
+    """Internal error reported when HTTP 404 is received while downloading time series files."""
+
+
+_TimeSeriesQueue: TypeAlias = asyncio.Queue[tuple[str, bytes]]
+_SavingTasks: TypeAlias = list[tuple[str, asyncio.Future[None]]]
+
+
 class KeplerDownloader:
-    def __init__(self, saver: Saver, nasa_base_url: str, mast_base_url: str) -> None:
+    """Downloader for fetching and saving Kepler table and time series files.
+
+    Tables in CSV format and time series in FITS format are fetched asynchronously via REST API
+    from the official archives of NASA and the Mikulski Archive for Space Telescopes (MAST) and
+    then saved to the local or external destination.
+    """
+
+    def __init__(
+        self,
+        saver: Saver,
+        cadence: Cadence,
+        nasa_base_url: str,
+        mast_base_url: str,
+        num_tasks: int = 40,
+    ) -> None:
         self._saver = saver
         self._nasa_base_url = nasa_base_url.rstrip("/")
         self._mast_base_url = mast_base_url.rstrip("/")
-        home = Path().home()
-        self._tables_meta_path = home / f"{self.__class__.__name__}_tables.txt"
-        self._series_meta_path = home / f"{self.__class__.__name__}_series.txt"
+        self._urls_meta_path = Path().home() / f"{self.__class__.__name__}_series.txt"
+        self._cadence = cadence
+        self._num_tasks = num_tasks
+        self._missing_file_urls: list[str] = []
+        self._saved_file_urls: list[str] = []
 
     async def download_tables(self, requests: Iterable[TableRequest]) -> None:
-        """Download NASA tables from the official archive to the local or external file system
-        (e.g. HDFS or GCS).
+        """Download tables in CSV files from the NASA archive.
 
-        Arguments:
-            requests (Iterable [TableRequest]): Requests that specify what tables are download
+        Downloaded files are saved to the local or external file system e.g. HDFS, AWS etc.
+
+        Args:
+            requests (Iterable [TableRequest]): Requests specifying which tables to download
         """
         async with aiohttp.ClientSession() as sess:
             for request in requests:
@@ -63,12 +92,34 @@ class KeplerDownloader:
                     table = await download(self._create_table_url(request), sess)
                     self._raise_for_nasa_error(table)
                 except ApiError as ex:
-                    log.warning("Cannot download NASA table", reason=ex)
+                    log.warning("Cannot download NASA table", reason=ex, table=request.name)
                     continue
                 try:
                     self._saver.save_table(request.name, table)
                 except Exception as ex:
-                    log.warning("Cannot save NASA table", reason=ex)
+                    log.warning("Cannot save NASA table", reason=ex, table=request.name)
+
+    async def download_time_series(self, ids: Iterable[int]) -> None:
+        """Download time series in FITS files from the MAST archive.
+
+        Downloaded files are saved in a local or external file system, e.g. HDFS, AWS, etc.
+        Fetching and saving are done asynchronously with the retry strategy for download errors.
+        When all retries have failed, the original error is re-raised to the caller. Any save error
+        is immediately re-raised.
+
+        This method implements a mechanism to prevent re-downloading of already downloaded files.
+        URLs that have been processed are saved locally in the metadata text file.
+
+        Args:
+            ids (Iterable[int]): The IDs of the targets for which the time series are downloaded
+        """
+        queue: _TimeSeriesQueue = asyncio.Queue(self._num_tasks)
+        downloading_task = self._fetch_time_series(ids, queue)
+        saving_task = asyncio.create_task(self._save_time_series(queue))
+        await downloading_task
+        await queue.join()
+        saving_task.cancel()
+        await saving_task
 
     def _raise_for_nasa_error(self, response_body: bytes) -> None:
         # Error from NASA API is of format:
@@ -87,4 +138,135 @@ class KeplerDownloader:
         table = f"table={request.name}"
         select = f"&select={','.join(request.columns)}" if request.columns else ""
         query = f"&where={request.query}" if request.query else ""
-        return f"{self._nasa_base_url}?{table}{select}{query}&format=csv"
+        return f"{self._nasa_base_url}?{table}{select}{query}&format=csv"  # Only csv for now
+
+    async def _save_time_series(self, queue: _TimeSeriesQueue) -> None:
+        saving_tasks_len = self._num_tasks * 2
+        saving_tasks: _SavingTasks = []
+        loop = asyncio.get_running_loop()
+
+        with concurrent.futures.ThreadPoolExecutor(self._num_tasks) as pool:
+            try:
+                while True:
+                    url, data = await self._get_queue_item(queue)
+                    self._add_save_task(saving_tasks, loop, pool, url, data)
+
+                    if len(saving_tasks) == saving_tasks_len:
+                        await self._await_save(saving_tasks)  # pragma: no cover
+            except asyncio.CancelledError:
+                # After cancelling, start save tasks for any files not yet saved
+                for _ in range(queue.qsize()):  # pragma: no cover
+                    self._add_save_task(saving_tasks, loop, pool, *queue.get_nowait())
+                if saving_tasks:
+                    await self._await_save(saving_tasks)
+            finally:
+                self._save_meta(self._saved_file_urls)
+
+    def _add_save_task(
+        self,
+        tasks: _SavingTasks,
+        loop: asyncio.AbstractEventLoop,
+        pool: concurrent.futures.ThreadPoolExecutor,
+        url: str,
+        data: bytes,
+    ) -> None:
+        filename = url.split("/")[-1]
+        item = filename, loop.run_in_executor(pool, self._saver.save_time_series, filename, data)
+        tasks.append(item)
+
+    async def _get_queue_item(self, queue: _TimeSeriesQueue) -> tuple[str, bytes]:
+        url, data = await queue.get()
+        queue.task_done()
+        return url, data
+
+    async def _await_save(self, tasks: _SavingTasks) -> None:
+        urls, save_tasks = list(zip(*tasks))
+        results = await asyncio.gather(*save_tasks, return_exceptions=True)
+        tasks.clear()
+        for url, result in zip(urls, results):
+            self._handle_saving_result(result, url)
+
+    @functools.singledispatchmethod
+    def _handle_saving_result(self, result: None, url: str) -> None:
+        log.info("FITS file save sucessful", url=url)
+        self._saved_file_urls.append(url)
+
+    @_handle_saving_result.register
+    def _(self, result: Exception, url: str) -> None:
+        # Is this log even necessary?
+        log.error("Cannot save FITS files", reason=result, url=url)
+        raise result
+
+    async def _fetch_time_series(self, ids: Iterable[int], queue: _TimeSeriesQueue) -> None:
+        log.info("Start downloading FITS files")
+        self._urls_meta_path.touch(exist_ok=True)
+        urls = self._filter_urls(list(self._create_mast_urls(ids)))
+
+        try:
+            for url_batch in self._url_batches(urls):
+                tasks = [self._fetch(url) for url in url_batch]
+                results = await asyncio.gather(*tasks, return_exceptions=True)
+
+                for url, result in zip(url_batch, results):
+                    await self._handle_fetch_result(result, url, queue)
+        finally:
+            self._save_meta(self._missing_file_urls)
+
+    @functools.singledispatchmethod
+    async def _handle_fetch_result(self, result: bytes, url: str, queue: _TimeSeriesQueue) -> None:
+        log.info("FITS file download successful", url=url)
+        await queue.put((url, result))
+
+    @_handle_fetch_result.register
+    async def _(self, result: Exception, url: str, queue: _TimeSeriesQueue) -> None:
+        log.error("Cannot download FITS file", reason=result)
+        raise result
+
+    @_handle_fetch_result.register
+    async def _(self, result: _MissingFileError, url: str, queue: _TimeSeriesQueue) -> None:
+        log.info("Missing FITS file", url=url)
+        self._missing_file_urls.append(url)
+
+    @retry(on=ApiError)
+    async def _fetch(self, url: str) -> bytes:
+        async with aiohttp.ClientSession() as sess:
+            try:
+                return await download(url, sess)
+            except ApiError as ex:
+                if ex.status == 404:
+                    raise _MissingFileError
+                raise
+            except asyncio.TimeoutError:
+                # In case of aiohttp session timeout (300s by default)
+                raise ApiError("HTTP request timeout", 408, url)
+
+    def _create_mast_urls(self, ids: Iterable[int]) -> Iterator[str]:
+        log.info("Creating MAST HTTP URLs")
+        # This produces urls like e.g:
+        # https://mast.stsci.edu/api/v0.1/Download/file?uri=mast:Kepler/url/missions/kepler/lightcurves/0008/000892376//kplr000892376-2009166043257_llc.fits
+        for id_ in ids:
+            id_str = f"{id_:09d}"
+            url = f"{self._mast_base_url}/{id_str[:4]}/{id_str}//kplr{id_str}"
+            yield from (
+                f"{url}-{pref}_{self._cadence.value}.fits"
+                for pref in get_quarter_prefixes(self._cadence)
+            )
+
+    def _filter_urls(self, urls: Iterable[str]) -> list[str]:
+        processed_urls = set(self._urls_meta_path.read_text().splitlines())
+        if processed_urls:
+            meta_path = self._urls_meta_path.as_posix()
+            log.info("Metadata file detected. Skip URLs from this file", path=meta_path)
+        return list(set(urls) - processed_urls)
+
+    def _save_meta(self, urls: list[str]) -> None:
+        if urls:
+            text = "\n".join(urls) + "\n"
+            filepath = self._urls_meta_path.as_posix()
+            with open(filepath, mode="a") as f:
+                f.write(text)
+            log.info("Metadata file saved", number_of_urls=len(urls), path=filepath)
+
+    def _url_batches(self, urls: list[str]) -> Iterator[Iterable[str]]:
+        log.info("Creating URLs batches", batch_size=self._num_tasks)
+        yield from (urls[n : n + self._num_tasks] for n in range(0, len(urls), self._num_tasks))
--- tests/downloaders_test.py
+++ tests/downloaders_test.py
@@ -1,8 +1,10 @@
-from unittest.mock import ANY, MagicMock
+import asyncio
+from unittest.mock import ANY, MagicMock, call
 
 import pytest
 
 from gaia.downloaders import KeplerDownloader, TableRequest
+from gaia.enums import Cadence
 from gaia.http import ApiError
 from gaia.io import FileSaver
 
@@ -12,17 +14,28 @@ MAST_BASE_URL = "https://www.mast.com"
 
 
 @pytest.fixture
-def downloader():
-    """Return an instance of `KeplerDownloader` with mocked `FileSaver`."""
+def downloader(tmp_path):
+    """Return a test instance of `KeplerDownloader` class.
+
+    The object has mocked `FileSaver`, test metadata path and `Cadence.LONG` as cadence.
+    """
     saver = MagicMock(spec=FileSaver)
-    return KeplerDownloader(saver=saver, nasa_base_url=NASA_BASE_URL, mast_base_url=MAST_BASE_URL)
+    instance = KeplerDownloader(
+        saver=saver,
+        nasa_base_url=NASA_BASE_URL,
+        mast_base_url=MAST_BASE_URL,
+        cadence=Cadence.LONG,
+    )
+    test_meta_path = tmp_path / "test_meta.txt"
+    instance._urls_meta_path = test_meta_path
+    return instance
 
 
 @pytest.mark.asyncio
 async def test_download_tables__no_failure_on_single_table_download_error(mocker, downloader):
-    """Test that a single table downloading error doesn't interrupt the entire process."""
+    """Test that a single table downloading error does not interrupt the entire process."""
     requests = [TableRequest("tab1"), TableRequest("tab2")]
-    # Error on second tables downloading.
+    # Error on second tables downloading
     mocker.patch(
         "gaia.downloaders.download",
         side_effect=[ApiError("test error", 500), b"test data"],
@@ -32,10 +45,10 @@ async def test_download_tables__no_failure_on_single_table_download_error(mocker
 
 @pytest.mark.asyncio
 async def test_download_tables__no_failure_on_single_table_saving_error(mocker, downloader):
-    """Test that a single table saving error doesn't interrupt the entire process."""
+    """Test that a single table saving error does not interrupt the entire process."""
     requests = [TableRequest("tab1"), TableRequest("tab2")]
     mocker.patch("gaia.downloaders.download", side_effect=[b"test data", b"test data"])
-    # Error on second table saving.
+    # Error on second table saving
     downloader._saver.save_table.side_effect = [Exception("test error"), None]
     await downloader.download_tables(requests)
 
@@ -46,7 +59,7 @@ async def test_download_tables__no_failure_on_single_table_saving_error(mocker, 
     [
         (TableRequest("table1"), f"{NASA_BASE_URL}?table=table1&format=csv"),
         (
-            TableRequest("table1", columns={"col1", "col2"}),
+            TableRequest("table1", columns=["col1", "col2"]),
             f"{NASA_BASE_URL}?table=table1&select=col1,col2&format=csv",
         ),
         (
@@ -54,7 +67,7 @@ async def test_download_tables__no_failure_on_single_table_saving_error(mocker, 
             f"{NASA_BASE_URL}?table=table1&where=col1 is null&format=csv",
         ),
         (
-            TableRequest("table1", columns={"col1", "col2"}, query="col1 is null"),
+            TableRequest("table1", columns=["col1", "col2"], query="col1 is null"),
             f"{NASA_BASE_URL}?table=table1&select=col1,col2&where=col1 is null&format=csv",
         ),
     ],
@@ -66,16 +79,164 @@ async def test_download_tables__no_failure_on_single_table_saving_error(mocker, 
     ],
 )
 async def test_download_tables__request_correct_url(table_request, url, mocker, downloader):
-    """Test check whether ."""
+    """Test that download function is called with the correct URL."""
     download_mock = mocker.patch("gaia.downloaders.download", return_value=b"test data")
     await downloader.download_tables([table_request])
     download_mock.assert_called_with(url, ANY)
 
 
 @pytest.mark.asyncio
-@pytest.mark.usefixtures("setup_logging")
-async def test_download_tables__treat_http_200_error_body_as_error(mocker, caplog, downloader):
-    """Test check whether ."""
+async def test_download_tables__treat_http_200_error_body_as_error(mocker, downloader):
+    """Test that the response beginning with 'ERROR<br>' is treated as an error and not saved."""
     mocker.patch("gaia.downloaders.download", return_value=b"ERROR<br>")
     await downloader.download_tables([TableRequest("tab1")])
-    assert "Cannot download NASA table" in caplog.text
+    downloader._saver.save_table.assert_not_called()
+
+
+URL_A_PREF = "https://www.mast.com/0000/000000001//kplr000000001-a_pref_llc.fits"
+URL_B_PREF = "https://www.mast.com/0000/000000001//kplr000000001-b_pref_llc.fits"
+
+
+@pytest.fixture
+def downloader_with_test_meta(downloader):
+    """
+    Return `downloader` fixture result but with test URL
+    https://www.mast.com/0000/000000001//kplr000000001-a_pref_llc.fits saved in the metadata file.
+    """
+    downloaded_url = f"{URL_A_PREF}\n"
+    downloader._urls_meta_path.write_text(downloaded_url)
+    return downloader
+
+
+TEST_ID = (1,)
+
+
+@pytest.mark.asyncio
+async def test_download_time_series__skip_already_downloaded_files(
+    mocker,
+    downloader_with_test_meta,
+):
+    """Test that already downloaded FITS files are not being re-downloaded."""
+    mocker.patch("gaia.downloaders.get_quarter_prefixes", return_value=("a_pref", "b_pref"))
+    download_mock = mocker.patch("gaia.downloaders.download", side_effect=[b"test", b"test"])
+    await downloader_with_test_meta.download_time_series(TEST_ID)
+    download_mock.assert_called_once_with(URL_B_PREF, ANY)
+
+
+@pytest.mark.asyncio
+async def test_download_time_series__skip_missing_file(mocker, downloader_with_test_meta):
+    """Test that missing FITS files are skipped (no errors raised and data saved)."""
+    mocker.patch("gaia.downloaders.get_quarter_prefixes", return_value=("a_pref", "b_pref"))
+    mocker.patch("gaia.downloaders.download", return_value=b"test")
+    await downloader_with_test_meta.download_time_series(TEST_ID)
+    downloader_with_test_meta._saver.save_time_series.assert_called_once_with(
+        "kplr000000001-b_pref_llc.fits",
+        b"test",
+    )
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("cadence", [Cadence.LONG, Cadence.SHORT])
+async def test_download_time_series__request_correct_url(cadence, mocker, downloader):
+    """Test that download function is called with the correct URL."""
+    downloader._cadence = cadence
+    mocker.patch("gaia.downloaders.get_quarter_prefixes", return_value=("a_pref", "b_pref"))
+    download_mock = mocker.patch("gaia.downloaders.download", side_effect=[b"test", b"test"])
+    download_calls = [
+        call(
+            f"https://www.mast.com/0000/000000001//kplr000000001-a_pref_{cadence.value}.fits",
+            ANY,
+        ),
+        call(
+            f"https://www.mast.com/0000/000000001//kplr000000001-b_pref_{cadence.value}.fits",
+            ANY,
+        ),
+    ]
+    await downloader.download_time_series(TEST_ID)
+    download_mock.assert_has_calls(download_calls, any_order=True)
+
+
+@pytest.mark.asyncio
+async def test_download_time_series__save_downloaded_files(mocker, downloader):
+    """Test that downloaded FITS files are saved (saving method called with correct args)."""
+    test_data = b"test"
+    mocker.patch("gaia.downloaders.get_quarter_prefixes", return_value=("a_pref", "b_pref"))
+    mocker.patch("gaia.downloaders.download", side_effect=[test_data, test_data])
+    save_calls = [
+        call("kplr000000001-a_pref_llc.fits", test_data),
+        call("kplr000000001-b_pref_llc.fits", test_data),
+    ]
+    await downloader.download_time_series(TEST_ID)
+    downloader._saver.save_time_series.assert_has_calls(save_calls, any_order=True)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "http_responses",
+    [
+        (b"test", b"test"),
+        (ApiError("missing file", 404), ApiError("missing file", 404)),
+        (ApiError("missing file", 404), b"test"),
+        (b"test", ApiError("missing file", 404)),
+    ],
+    ids=["two_files", "two_missing_files", "missing_and_file", "file_and_missing"],
+)
+async def test_download_time_series__save_meta(http_responses, mocker, downloader):
+    """Test that URLs are properly saved to the metadata file."""
+    mocker.patch("gaia.downloaders.get_quarter_prefixes", return_value=("a_pref", "b_pref"))
+    mocker.patch("gaia.downloaders.download", side_effect=http_responses)
+    await downloader.download_time_series(TEST_ID)
+    downloader._urls_meta_path.read_text().splitlines() == [URL_A_PREF, URL_B_PREF]
+
+
+@pytest.fixture
+def disable_retry_sleep(mocker):
+    """Mock `asyncio.sleep` in `gaia.utils module`."""
+    mocker.patch("gaia.utils.asyncio.sleep")  # Disable async sleeping in retry decorator
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures("disable_retry_sleep")
+async def test_download_time_series__retrying_failed_requests(mocker, downloader):
+    """
+    Test that the method doesn't break when a single download fails but the download is retry the
+    specified number of times.
+    """
+    # NOTE: This test asumes that retries number == 5 (default) and retry on ApiError for _fetch()
+    mocker.patch("gaia.downloaders.get_quarter_prefixes", return_value=("a_pref",))
+    download_results = [
+        ApiError("test error", 503),
+        ApiError("test error", 503),
+        asyncio.TimeoutError(),
+        asyncio.TimeoutError(),
+        ApiError("test error", 503),
+        b"ok",
+    ]
+    # 1 normal call + 5 retries
+    download_mock = mocker.patch("gaia.downloaders.download", side_effect=download_results)
+    await downloader.download_time_series(TEST_ID)
+    assert download_mock.await_count == 6
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures("disable_retry_sleep")
+async def test_download_time_seris__raise_when_ratries_limit_reached(mocker, downloader):
+    """Test that an error is reported after exceeding the download retry limit."""
+    # NOTE: This test asumes that retries number == 5 (default) and retry on ApiError for _fetch()
+    mocker.patch("gaia.downloaders.get_quarter_prefixes", return_value=("a_pref",))
+    # 1 normal call + 5 retries end up with an error
+    download_results = [ApiError("test error", 503)] * 6
+    mocker.patch("gaia.downloaders.download", side_effect=download_results)
+    with pytest.raises(ApiError, match="test error"):
+        await downloader.download_time_series(TEST_ID)
+
+
+@pytest.mark.asyncio
+async def test_download_time_series__raise_on_file_saving_error(mocker, downloader):
+    """Test that any error raised by time series saving method is re-raised to the caller."""
+    mocker.patch("gaia.downloaders.get_quarter_prefixes", return_value=("a_pref",))
+    mocker.patch("gaia.downloaders.download", side_effect=b"ok")
+    saving_error_msg = "saving time series file error"
+    downloader._saver.save_time_series.side_effect = PermissionError(saving_error_msg)
+    with pytest.raises(PermissionError, match=saving_error_msg):
+        await downloader.download_time_series(TEST_ID)
