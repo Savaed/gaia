56c27e272ddd7539a46b761971599f79a9e84e20
Krzysztof Gonet <36476874+Savaed@users.noreply.github.com>
2023-02-05 17:14:30 +0100

    Add Kepler FITS time series files combiner

--- gaia/data/combiners.py
+++ gaia/data/combiners.py
@@ -9,142 +9,154 @@ is split into many files for each observation quarter for every single KOI.
 
 import asyncio
 import concurrent.futures
+import functools
 import re
 from collections import defaultdict
-from collections.abc import Iterable
-from functools import singledispatchmethod
-from typing import Any, Generic, Protocol, TypeAlias, TypedDict, TypeVar
+from collections.abc import AsyncIterator
+from pathlib import Path
+from typing import Any, Generic, Protocol, TypeAlias, TypeVar
 
-import numpy as np
 import structlog
 import tensorflow as tf
+from rich.progress import track
 
 from gaia.io import read_fits_table
 
 
 log = structlog.stdlib.get_logger()
 
-T = TypeVar("T", bound=dict[str, Any])
-
-# Represents time series observed at specific periods, e.g. quarters for Kepler time series
-IntervalTimeSeries: TypeAlias = dict[str, dict[str, T]]
-
-
-class KeplerTimeSeries(TypedDict):
-    TIME: list[np.ndarray]
-    SAP_FLUX: list[np.ndarray]
-    PDCSAP_FLUX: list[np.ndarray]
+T = TypeVar("T", bound=dict[Any, Any])
 
 
 class CombinerError(Exception):
-    """Raised when cannot process object(s) in combining."""
+    """Raised when the combining process cannot be fully completed."""
 
 
-class FilesCombiner(Protocol[T]):
-    async def combine(self, paths: Iterable[str]) -> IntervalTimeSeries[T]:
+class Combiner(Protocol[T]):  # type: ignore
+    async def combine(self, fields: set[str] | None = None) -> AsyncIterator[tuple[str, T]]:
         ...
 
 
-class FitsCombiner(Generic[T]):
-    """
-    Provides a convenient way to combine tabular data from multiple FITS files into a single output
-    the dictionary uses the `astropy` module to read
-    """
+# This is in the format: kepid -> list[tuple[quarter, filepath]]
+_Groups: TypeAlias = dict[str, list[tuple[str, str]]]
+
+
+class KeplerFitsCombiner(Generic[T]):
+    """File combiner to combine multiple time series FITS files for a single target (KOI)."""
 
     def __init__(
         self,
+        data_dir: str,
+        id_pattern: str,
         quarter_pattern: str,
         hdu_header: str = "LIGHTCURVE",
-        fields: Iterable[str] | None = None,
     ) -> None:
-        super().__init__()
-        self._fields = set(fields) if fields else None
+        self._data_dir = data_dir
+        self._kepid_pattern = re.compile(id_pattern)
         self._quarter_pattern = re.compile(quarter_pattern)
-        self._hdu_header = hdu_header
+        self._header = hdu_header
+        self._meta_path = Path().home() / f"{self.__class__.__name__}_metadata.txt"
 
-    async def combine(self, paths: list[str]) -> dict[str, T]:
-        """Combine tables from multiple FITS files read from `paths` into a single dictionary.
+    async def combine(self, fields: set[str] | None = None) -> AsyncIterator[tuple[str, T]]:
+        """Combine multiple FITS files into one dictionary for each KOI.
+
+        This function combines multiple files for one KOI into dictionaries in the format:
+        {'quarter_prefix': 'T'}', where 'T' is data format in FITS files. Moreover it checks
+        files already combined (from the metadata text file) and does not process them again.
 
         Args:
-            paths (Iterable[str]): Paths to FITS files. It should be all paths for a single object
+            fields (set[str] | None, optional): Fields to keep in the output dictionaries.
+                If None, all available fields will be kept. Default None.
 
         Raises:
-            CombinerError: FITS file reading error, connection error (for external resources like
-                GCS) or ID/quarter extraction error
+            CombinerError: The fields passed to the `fields` parameter and the fields from the FITS
+                files do not match OR the file HDU extension is missing OR the file paths cannot be
+                grouped by KOI and/or quarter prefixes.
 
-        Returns:
-            dict[str, T]: Dictionary with combined data from all files
+        Yields:
+            Iterator[AsyncIterator[tuple[str, T]]: A tuple with KOI ID and dict with combined data
+                for each KOI.
         """
+        self._meta_path.touch(exist_ok=True)
         loop = asyncio.get_running_loop()
-        quarters_prefixes, filtered_paths = self._extract_quarter_prefixes(list(paths))
 
         with concurrent.futures.ThreadPoolExecutor() as pool:
-            tasks = [
-                loop.run_in_executor(pool, read_fits_table, path, self._hdu_header, self._fields)
-                for path in filtered_paths
-            ]
-            results = await asyncio.gather(*tasks, return_exceptions=True)
-            out: dict[str, T] = {}
-            for prefix, data in zip(quarters_prefixes, results):
-                self._handle_result(data, prefix, out)
+            for kepid, paths_group in track(self._group_paths().items(), "Combining FITS files"):
+                output: T = defaultdict(dict)  # type: ignore
+                quarter_prefixes, paths = list(zip(*paths_group))
+                tasks = self._start_read_tasks(fields, loop, paths, pool)
+                results = await asyncio.gather(*tasks, return_exceptions=True)
 
-            if not out:
-                raise CombinerError("File reading failed for all paths")
+                for prefix, result in zip(quarter_prefixes, results):
+                    self._handle_read_result(result, prefix, output)
 
-            return out
+                log.debug("File combine succeeded", kepid=kepid)
+                yield kepid, output
+                self._save_meta(kepid)
 
-    @singledispatchmethod
-    def _handle_result(self, result: T, prefix: str, out: dict[str, T]) -> None:
-        out[prefix] = result
+    def _save_meta(self, kepid: str) -> None:
+        with open(self._meta_path, mode="a") as f:
+            f.write(f"{kepid}\n")
+        log.debug("Metadata saved", kepid=kepid)
 
-    @_handle_result.register
-    def _(self, result: Exception, prefix: str, out: dict[str, T]) -> None:
-        log.warning(str(result))
+    @functools.singledispatchmethod
+    def _handle_read_result(
+        self,
+        result: T,
+        quarter_prefix: str,
+        output: dict[Any, Any],
+    ) -> None:
+        output[quarter_prefix] = result
 
-    def _extract_quarter_prefixes(self, paths: list[str]) -> tuple[list[str], list[str]]:
-        prefixes: list[str] = []
-        filtered_paths = paths.copy()
+    @_handle_read_result.register
+    def _(self, result: Exception, quarter_prefix: str, output: dict[Any, Any]) -> None:
+        raise CombinerError(result)
 
-        for path in paths:
-            try:
-                prefixes.append(self._quarter_pattern.search(path).group())  # type: ignore
-            except AttributeError:
-                msg = (
-                    f"Cannot retrieve quarter prefix from {path!r} using "
-                    f"pattern {self._quarter_pattern.pattern!r}"
-                )
-                log.warning(msg)
-                filtered_paths.remove(path)
+    def _start_read_tasks(
+        self,
+        fields: set[str] | None,
+        loop: asyncio.AbstractEventLoop,
+        paths: list[str],
+        pool: concurrent.futures.ThreadPoolExecutor,
+    ) -> list[asyncio.Future[dict[str, Any]]]:
+        return [loop.run_in_executor(pool, read_fits_table, p, self._header, fields) for p in paths]
 
-        if not any(prefixes):
-            raise CombinerError("Quarter prefix extraction failed for all paths")
+    def _group_paths(self) -> _Groups:
+        if not tf.io.gfile.exists(self._data_dir):
+            raise CombinerError(f"Root data directory {self._data_dir!r} not found")
 
-        return prefixes, filtered_paths
+        groups: _Groups = defaultdict(list)
+        processed_kepids = self._read_meta()
 
+        log.info("Grouping paths based on kepid", data_dir=self._data_dir)
+        for path in tf.io.gfile.listdir(self._data_dir):
+            kepid = self._substract_from_path(path, self._kepid_pattern)
 
-def group_kepler_paths(data_dir: str, kepid_pattern: str) -> dict[str, list[str]]:
-    """Group Kepler time series FITS file paths into the dictionary where KIC (kepid) is the key.
+            if kepid in processed_kepids:
+                continue
 
-    Args:
-        data_dir (str): Root data directory path
-        kepid_pattern (str): Regex pattern to extract kepid from file paths
+            quarter_prefix = self._substract_from_path(path, self._quarter_pattern)
+            # `tf.io.gfile.listdir()` returns only filenames, but we need full paths
+            path = f"{self._data_dir}/{path}"
+            groups[kepid].append((quarter_prefix, path))
 
-    Returns:
-        dict[str, list[str]]: Dictionary 'kepid' -> ['file_path1', 'file_path2', ...]
-    """
-    log.info("Grouping paths based on kepid", data_dir=data_dir)
-    groups = defaultdict(list)
-    paths = tf.io.gfile.glob(f"{data_dir.rstrip('/')}/*.fits")
-    pattern = re.compile(kepid_pattern)
-    log.info(f"Found {len(paths)} paths")
+        log.info(f"Paths splited into {len(groups)} group(s)")
+        return groups
 
-    for path in paths:
+    def _read_meta(self) -> set[str]:
+        processed_kepids = set(self._meta_path.read_text().splitlines())
+        if processed_kepids:
+            log.info(
+                "Metadata file detected. Skip processed files",
+                path=str(self._meta_path),
+                kepids_len=len(processed_kepids),
+            )
+        return processed_kepids
+
+    def _substract_from_path(self, path: str, pattern: re.Pattern[str]) -> str:
         try:
-            kepid = pattern.search(path).group()  # type: ignore[union-attr]
+            return pattern.search(path).group()  # type: ignore[union-attr]
         except AttributeError:
-            log.warning(f"Cannot retrieve kepid from {path!r} using a pattern {kepid_pattern!r}")
-        else:
-            groups[kepid].append(path)
-
-    log.info(f"Paths grouped into {len(groups)} groups")
-    return groups
+            raise CombinerError(
+                f"Cannot substract quarter prefix or kepid from {path=!r} using {pattern=!r}",
+            )
--- tests/combiners_test.py
+++ tests/combiners_test.py
@@ -1,132 +1,121 @@
-from unittest.mock import Mock
+import re
+from typing import Any
 
 import pytest
+import tensorflow.python.framework.errors_impl as tf_errors
 
-from gaia.data.combiners import CombinerError, FitsCombiner, group_kepler_paths
-from tests.conftest import assert_dict_with_list_equal_no_order
-
-
-class TestKeplerFitsCombiner:
-    SINGLE_FITS_DICT = {"col1": 1, "col2": 2}
-
-    @pytest.fixture
-    def combiner(self):
-        """Return `gaia.data.combiners.KeplerFitsCombiner` instance."""
-        return FitsCombiner(r"(?=.*)\d$")
-
-    @pytest.fixture
-    def paths(self):
-        """Return FITS file paths."""
-        return ["path1", "path2", "path3"]
-
-    @pytest.mark.asyncio
-    async def test_combine__no_quarter_extracted(self, combiner, paths):
-        combiner._quarter_pattern = Mock(search=Mock(side_effect=[None] * len(paths)))
-        with pytest.raises(CombinerError, match="Quarter prefix extraction failed for all paths"):
-            await combiner.combine(paths)
-
-    @pytest.mark.asyncio
-    async def test_combine__no_file_read(self, combiner, paths, mocker):
-        """Test check whether CombinerError is raised when no FITS file can be read."""
-        mocker.patch("gaia.data.combiners.tf.io.gfile.glob", return_value=paths)
-        mocker.patch(
-            "gaia.data.combiners.read_fits_table",
-            side_effect=[Exception("error")] * len(paths),
-        )
-        with pytest.raises(CombinerError, match="File reading failed for all paths"):
-            await combiner.combine(paths)
-
-    @pytest.mark.asyncio
-    @pytest.mark.parametrize(
-        "prefs,read_outputs,expected",
-        [
-            (
-                [AttributeError(), "2", "3"],
-                [SINGLE_FITS_DICT, SINGLE_FITS_DICT],
-                {"2": SINGLE_FITS_DICT, "3": SINGLE_FITS_DICT},
-            ),
-            (
-                [AttributeError(), "2", "3"],
-                [SINGLE_FITS_DICT, Exception()],
-                {"2": SINGLE_FITS_DICT},
-            ),
-            (
-                ["1", "2", "3"],
-                [SINGLE_FITS_DICT, Exception(), SINGLE_FITS_DICT],
-                {"1": SINGLE_FITS_DICT, "3": SINGLE_FITS_DICT},
-            ),
-        ],
-        ids=["prefix_error", "prefix_and_reading_error", "reading_error"],
-    )
-    async def test_combine__some_quarters_and_files_not_processed(
-        self,
-        prefs,
-        read_outputs,
-        expected,
-        combiner,
-        paths,
-        mocker,
-    ):
-        """Test check whether a correct dictionary is return even if not all files were read."""
-        mocker.patch("gaia.data.combiners.tf.io.gfile.glob", return_value=paths)
-        combiner._quarter_pattern = Mock(**{"search.return_value.group.side_effect": prefs})
-        mocker.patch("gaia.data.combiners.read_fits_table", side_effect=read_outputs)
-        result = await combiner.combine(paths)
-        assert result == expected
-
-    @pytest.mark.asyncio
-    async def test_combine__return_dictionary_no_error(self, combiner, mocker, paths):
-        """Test check whether the correct dictionary is returned when there is no error."""
-        mocker.patch("gaia.data.combiners.tf.io.gfile.glob", return_value=paths)
-        mocker.patch(
-            "gaia.data.combiners.read_fits_table",
-            side_effect=[self.SINGLE_FITS_DICT] * len(paths),
-        )
-        expected = dict.fromkeys(("1", "2", "3"), self.SINGLE_FITS_DICT)
-        result = await combiner.combine(paths)
-        assert result == expected
+from gaia.data.combiners import CombinerError, KeplerFitsCombiner
 
 
-@pytest.mark.parametrize("directory", ["", "abc"], ids=["empty", "not_found"])
-def test_group_kepler_paths__directory_not_found_or_empty(directory):
-    """
-    Test check whether an empty dictionary is returned when root data directory is not found or
-    empty.
-    """
-    result = group_kepler_paths(directory, r"(?<=kplr)\d{9}")
-    assert not result
+TEST_FITS_PATHS = ("kepid1-quarter1.fits", "kepid1-quarter2.fits", "kepid2-quarter1.fits")
+TEST_FITS_DICT = {"col1": [1, 2, 3]}
 
 
 @pytest.fixture
-def tmp_dir(tmp_path):
-    """Prepare a temporary directory with empty test FITS files."""
-    (tmp_path / "kplr123456789-123456789_llc.fits").touch()
-    (tmp_path / "kplr123456789-111111111_slc.fits").touch()
-    (tmp_path / "kplr987654321-987654321_llc.fits").touch()
-    return tmp_path
+def combined_dicts():
+    """Return a list of dictionaries which represents combined FITS files for `TEST_FITS_PATHS`."""
+    return [
+        ("kepid1", {"quarter1": TEST_FITS_DICT, "quarter2": TEST_FITS_DICT}),
+        ("kepid2", {"quarter1": TEST_FITS_DICT}),
+    ]
+
+
+@pytest.fixture
+def combiner(tmp_path):
+    """Return an instance of `KeplerFitsCombiner` with temporal (for each test) metafile path."""
+    instance = KeplerFitsCombiner[Any](
+        data_dir="a/b/c",
+        id_pattern=r"kepid\d",
+        quarter_pattern=r"quarter\d",
+    )
+    # Make sure that metadata is temporal for each test
+    instance._meta_path = tmp_path / "test_meta.txt"
+    return instance
 
 
 @pytest.mark.asyncio
-async def test_group_kepler_paths__cannot_retrieve_kepid(tmp_dir):
-    """Test check whether paths for which cannot retrieve kepid are not included in the output."""
-    result = group_kepler_paths(str(tmp_dir), r"llc")
-    expected = {
-        "llc": [
-            f"{tmp_dir}/kplr123456789-123456789_llc.fits",
-            f"{tmp_dir}/kplr987654321-987654321_llc.fits",
-        ],
-    }
-    assert_dict_with_list_equal_no_order(result, expected)
+async def test_combine__root_dir_not_found(mocker, combiner):
+    """Test that CombinerError is raised when no root data dir found."""
+    tf_not_found_error = tf_errors.NotFoundError(None, None, "Test directory not found")
+    mocker.patch("gaia.data.combiners.tf.io.gfile.listdir", side_effect=tf_not_found_error)
+    mocker.patch("gaia.data.combiners.tf.io.gfile.exists", return_value=False)
+    with pytest.raises(CombinerError, match="Root data directory"):
+        await anext(combiner.combine())
 
 
-def test_group_kepler_paths__return_grouped_paths(tmp_dir):
-    """Test check whether the paths are grouped correctly."""
-    result = group_kepler_paths(str(tmp_dir), r"(?<=kplr)\d{9}")
-    expected = {
-        "123456789": [
-            f"{tmp_dir}/kplr123456789-123456789_llc.fits",
-            f"{tmp_dir}/kplr123456789-111111111_slc.fits",
-        ],
-        "987654321": [f"{tmp_dir}/kplr987654321-987654321_llc.fits"],
-    }
-    assert_dict_with_list_equal_no_order(result, expected)
+@pytest.fixture
+def root_data_dir_exists(mocker):
+    """Mock `tf.io.gfile.exists()` to return `True` for root data directory."""
+    mocker.patch("gaia.data.combiners.tf.io.gfile.exists", return_value=True)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "error,error_msg",
+    [
+        (ValueError("Fields x not present in the FITS file"), "Fields .* not present"),
+        (KeyError("Extension not found"), ".*Extension not found.*"),
+    ],
+    ids=["missing_fits_fields", "invalid_fits_hdu_extension"],
+)
+@pytest.mark.usefixtures("root_data_dir_exists")
+async def test_combine__fits_files_read_error(error, error_msg, mocker, combiner):
+    """Test that CombinerError is raised when there is an error while reading FITS file(s)."""
+    mocker.patch("gaia.data.combiners.tf.io.gfile.listdir", return_value=[TEST_FITS_PATHS[0]])
+    mocker.patch("gaia.data.combiners.read_fits_table", side_effect=error)
+    with pytest.raises(CombinerError, match=error_msg):
+        await anext(combiner.combine())
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures("root_data_dir_exists")
+async def test_combine__yield_combined_dicts(mocker, combiner, combined_dicts):
+    """Test that properly combined files are yield as dictionaries."""
+    mocker.patch("gaia.data.combiners.tf.io.gfile.listdir", return_value=TEST_FITS_PATHS)
+    mocker.patch("gaia.data.combiners.read_fits_table", side_effect=[TEST_FITS_DICT] * 3)
+    results = [(kepid, combined_dict) async for kepid, combined_dict in combiner.combine()]
+    assert results == combined_dicts
+
+
+@pytest.fixture(params=["_kepid_pattern", "_quarter_pattern"], ids=["kepid", "quarter"])
+def invalid_pattern_combiner(request, combiner):
+    """Return an instance of `KeplerFitsCombiner` with invalivd kepid or quarter pattern."""
+    setattr(combiner, request.param, re.compile("^invalid_pattern$"))
+    return combiner
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures("root_data_dir_exists")
+async def test_combine__kepid_or_quarter_prefix_search_error(mocker, invalid_pattern_combiner):
+    """Test that CombinerError is raised when there is a search error (for kepid or quarter)."""
+    mocker.patch("gaia.data.combiners.tf.io.gfile.listdir", return_value=TEST_FITS_PATHS)
+    with pytest.raises(CombinerError, match="Cannot substract quarter prefix or kepid"):
+        await anext(invalid_pattern_combiner.combine())
+
+
+@pytest.fixture
+def combiner_with_meta_kepid1(combiner):
+    """Return an instance of `KeplerFitsCombiner` with ID 'kepid1' already processed."""
+    combiner._meta_path.write_text("kepid1\n")
+    return combiner
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures("root_data_dir_exists")
+async def test_combine__skip_already_combined_files(mocker, combiner_with_meta_kepid1):
+    """Test that files that were already combined are not reprocessed."""
+    mocker.patch("gaia.data.combiners.tf.io.gfile.listdir", return_value=TEST_FITS_PATHS)
+    mocker.patch("gaia.data.combiners.read_fits_table", side_effect=[TEST_FITS_DICT])
+    result = await anext(combiner_with_meta_kepid1.combine())
+    result == ("kepid2", {"quarter1": TEST_FITS_DICT})
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures("root_data_dir_exists")
+async def test_combine__save_metadata_to_file(mocker, combiner):
+    """Test that kepids for combined files are saved in the metadata file."""
+    mocker.patch("gaia.data.combiners.tf.io.gfile.listdir", return_value=TEST_FITS_PATHS)
+    mocker.patch("gaia.data.combiners.read_fits_table", side_effect=[TEST_FITS_DICT] * 3)
+    [_ async for _ in combiner.combine()]  # Just process files. We don't care about the results
+    saved_meta_kepids = set(combiner._meta_path.read_text().splitlines())
+    assert saved_meta_kepids == {"kepid1", "kepid2"}
