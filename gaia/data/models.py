4a5f5e20223e7e679264645dff39a8687d11994d
Krzysztof Gonet <36476874+Savaed@users.noreply.github.com>
2023-04-13 12:43:12 +0200

    change: redesign data models

--- gaia/data/models.py
+++ gaia/data/models.py
@@ -1,55 +1,13 @@
 import abc
-from abc import ABC
-from dataclasses import dataclass, fields, is_dataclass
+from dataclasses import dataclass
 from enum import Enum
-from typing import Any, Iterable, TypeAlias, TypedDict
+from typing import Iterable, TypeAlias, TypedDict
 
 import numpy as np
 import numpy.typing as npt
 
 
-class FromDictMixin(ABC):
-    """Abstract class that provides a method to map flat dictionary to the dataclass object."""
-
-    # TODO: Use '() -> Self' after upgrading to python 3.11
-    @classmethod
-    def from_flat_dict(cls, data: dict[str, Any], mapping: dict[str, str] | None = None) -> Any:
-        """Construct a dataclass from a flat (not nested) dictionary.
-
-        Args:
-            data (dict[str, Any]): Dictionary to map from
-            mapping (dict[str, str] | None, optional): Dictionary keys to class fields names map in
-                the format: 'dict_key' -> 'dataclass_field_name'. If `None` then assume that field
-                and keys have the the same names. Defaults to None.
-
-        Raises:
-            TypeError: Class is not dataclass
-            KeyError: Some required class fields are missing in dictionary data. It may be due to
-                invalid names mapping
-
-        Returns:
-            Self: An instance of the dataclass with data mapped
-        """
-        if not is_dataclass(cls):
-            raise TypeError(f"Class '{cls}' must be a dataclass")
-
-        mapping = mapping or {}
-        class_fields_names = [field.name for field in fields(cls)]
-
-        if mapping:
-            # Map all data-dict fields to new class names and filter out non-class keys.
-            new_data = {mapping.get(k, k): v for k, v in data.items()}
-            new_data = {k: v for k, v in new_data.items() if k in class_fields_names}
-        else:
-            new_data = {k: v for k, v in data.items() if k in class_fields_names}
-
-        try:
-            return cls(**new_data)
-        except TypeError as ex:
-            missing_keys = str(ex).split(":")[-1].strip()
-            raise KeyError(
-                f"Required __init__ parameters: {missing_keys} not found in data dictionary",
-            )
+Id: TypeAlias = str | int
 
 
 @dataclass
@@ -60,19 +18,6 @@ class PeriodicEvent:
     period: float
 
 
-ID: TypeAlias = int | str
-
-
-TCE_NO_NAME_FLAG = "NO_NAME"
-
-
-@dataclass
-class TargetRelatedObject:
-    """Object related to the target star, binary or multiple system."""
-
-    target_id: ID
-
-
 class TceLabel(Enum):
     PC = "PLANET_CANDIDATE"
     NTP = "NON_TRANSITING_PHENOMENA"
@@ -81,12 +26,12 @@ class TceLabel(Enum):
     FP = "FALSE_POSITIVE"
     """Generic non-PC label. Prefer AFP and NTP.
 
-    This should only be used when there is no method to determine whether TCE is AFP or NTP.
+    This should only be used when cannot determine whether TCE is AFP or NTP.
     """
 
 
 @dataclass
-class TCE(FromDictMixin, TargetRelatedObject):
+class TCE(abc.ABC):
     """Threshold-Crossing Event.
 
     A sequence of transit-like features in the flux time series of a given target that resembles
@@ -94,25 +39,21 @@ class TCE(FromDictMixin, TargetRelatedObject):
     further analysis.
     """
 
-    tce_id: ID
+    tce_id: Id
+    target_id: Id
     name: str | None
-    label: TceLabel
+    label: TceLabel | None
+    epoch: float
+    duration: float
+    period: float
 
     @property
     @abc.abstractmethod
     def event(self) -> PeriodicEvent:
         ...
 
-    @classmethod
-    def from_flat_dict(cls, data: dict[str, Any], mapping: dict[str, str] | None = None) -> Any:
-        tce: TCE = super().from_flat_dict(data, mapping)
 
-        # Label is saved as `TceLabel.name` so it must be casted to the actual `TceLabel` enum
-        tce.label = TceLabel[tce.label]  # type: ignore
-        return tce
-
-
-@dataclass(unsafe_hash=True)
+@dataclass
 class KeplerTCE(TCE):
     opt_ghost_core_aperture_corr: float
     opt_ghost_halo_aperture_corr: float
@@ -121,29 +62,24 @@ class KeplerTCE(TCE):
     radius: float
     fitted_period: float
     transit_depth: float
-    epoch: float
-    duration: float
-    period: float
     _normalize_duration: bool = True
 
+    # TODO: add test for this
     def __post_init__(self) -> None:  # pragma: no cover
-        self.target_id = int(self.target_id)
-        self.tce_id = int(self.tce_id)
-
-        if self.name == TCE_NO_NAME_FLAG:  # This is for easier bool(tce)
-            self.name = None
-
         if self._normalize_duration:
-            self.duration = round(self.duration / 24, 4)  # For Kepler 'duration' is in hours
+            # TCE 'duration' for NASA tables is in hours
+            self.duration = round(self.duration / 24, 4)
 
     @property
-    def event(self) -> PeriodicEvent:  # pragma: no cover
+    def event(self) -> PeriodicEvent:
         return PeriodicEvent(self.epoch, self.duration, self.period)
 
 
 @dataclass
-class StellarParameters(FromDictMixin, TargetRelatedObject):
+class StellarParameters:
     """Physical properties of the target star, binary or multiple system."""
+
+    id: Id
 
 
 @dataclass
@@ -153,23 +89,34 @@ class KeplerStellarParameters(StellarParameters):
     mass: float
     density: float
     surface_gravity: float
-    """log(g)"""
     metallicity: float
 
 
 Series: TypeAlias = npt.NDArray[np.float_]
 
 
-class TimeSeries(TypedDict):
-    TIME: Series
+class TimeSeriesBase(TypedDict):
+    """Minimum time series representation."""
+
+    id: Id
+    time: Series
+
+
+class PeriodicTimeSeries(TimeSeriesBase):
+    """Time series for a single observation period."""
+
+    period: str
+
+
+class TimeSeries(TimeSeriesBase):
+    """Basic time series for multiple observation periods"""
+
+    periods_mask: Iterable[str]
 
 
 class KeplerTimeSeries(TimeSeries):
-    MOM_CENTR1: Series
-    MOM_CENTR2: Series
-    PDCSAP_FLUX: Series
+    """Kepler time series for multiple observation periods."""
 
-
-PeriodicTimeSeries: TypeAlias = dict[str, TimeSeries]
-
-PeriodicData: TypeAlias = dict[str, Iterable[float]]
+    pdcsap_flux: Series
+    mom_centr1: Series
+    mom_centr2: Series
