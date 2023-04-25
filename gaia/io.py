661a3e8ce9322484a410cad59bfd6b3544b28e6f
Krzysztof Gonet <36476874+Savaed@users.noreply.github.com>
2023-04-25 15:17:40 +0200

    change: remove multiplatform write/read functions

--- gaia/io.py
+++ gaia/io.py
@@ -1,67 +1,8 @@
-"""I/O functions to use in local or cloud environments (GCP, AWS) or HDFS."""
-
-import io
-from enum import Enum
-from typing import Any, AnyStr, Protocol
-
-import tensorflow as tf
-from astropy.io import fits
-from tensorflow.python.framework import errors_impl as tf_errors
+from pathlib import Path
+from typing import Protocol, Sequence, TypeAlias
 
 
-class FileMode(Enum):
-    WRITE = "wb"
-    WRITE_BINARY = "w"
-    READ = "r"
-    READ_BINARY = "rb"
-    APPEND = "a"
-
-
-def read(src: str, mode: FileMode = FileMode.READ_BINARY) -> AnyStr:
-    """Read file content from local environment cloud (GCP, AWS) or HDFS.
-
-    Args:
-        src (str): Path to the source file
-        mode (FileMode, optional): Read mode. Defaults to `FileMode.READ_BINARY`.
-
-    Raises:
-        FileNotFoundError: File not found
-        PermissionError: No permissions for file or cloud environment
-
-    Returns:
-        AnyStr: File content as bytes or string
-    """
-    try:
-        with tf.io.gfile.GFile(src, mode.value) as gf:
-            return gf.read()  # type: ignore
-    except tf_errors.NotFoundError:
-        raise FileNotFoundError(src)
-    except tf_errors.PermissionDeniedError as ex:
-        raise PermissionError(ex)
-
-
-def write(dest: str, data: AnyStr, mode: FileMode = FileMode.WRITE_BINARY) -> None:
-    """Write data to a file in the local environment, cloud (GCP, AWS) or HDFS.
-
-    In some environments, the destination file will be created if it does not exist (e.g. GCP),
-    however in the local environment, this function will raise FileNotFoundError.
-
-    Args:
-        dest (str): Path to the destination file
-        data (AnyStr): Data to write, bytes or string
-        mode (FileMode, optional): Write mode. Defaults to `FileMode.WRITE_BINARY`.
-
-    Raises:
-        PermissionError: No permissions for file or cloud environment
-        FileNotFoundError: File not found (in the local environment)
-    """
-    try:
-        with tf.io.gfile.GFile(dest, mode.value) as gf:
-            gf.write(data)
-    except tf_errors.NotFoundError:
-        raise FileNotFoundError(dest)
-    except tf_errors.PermissionDeniedError as ex:
-        raise PermissionError(ex)
+Columns: TypeAlias = Sequence[str]
 
 
 class Saver(Protocol):
@@ -78,38 +19,9 @@ class FileSaver:
         self._time_series_dir = time_series_dir.rstrip("/")
 
     def save_table(self, name: str, data: bytes) -> None:
-        write(f"{self._tables_dir}/{name}", data)
+        destination = Path(self._tables_dir) / name
+        destination.write_bytes(data)
 
     def save_time_series(self, name: str, data: bytes) -> None:
-        write(f"{self._time_series_dir}/{name}", data)
-
-
-def read_fits_table(src: str, header: str, fields: set[str] | None = None) -> dict[str, Any]:
-    """Read the table from a FITS file under the specified `header`.
-
-    Args:
-        src (str): Path to the source file
-        header (str): FITS header (HDU extension) to read from
-        fields (set[str] | None, optional): Fields to preserve in the output dictionary.
-            If None or empty set then all available fields are read. Defaults to None.
-
-    Raises:
-        ValueError: Any of specified fields are not present in the FITS file
-        KeyError: FITS header not found
-
-    Returns:
-        dict[str, Any]: Table from FITS file as dictionary 'file_field' -> 'data'
-    """
-    with fits.open(io.BytesIO(read(src))) as hdul:  # type: ignore[arg-type]
-        data = hdul[header]
-
-        columns = {col.name for col in data.columns}
-
-        if fields:
-            not_existent_fields = fields - columns
-            if not_existent_fields:
-                raise ValueError(f"Fields {not_existent_fields} not present in the FITS file")
-        else:
-            fields = columns
-
-        return {col: data.data[col] for col in columns if col in fields}
+        destination = Path(self._time_series_dir) / name
+        destination.write_bytes(data)
--- tests/io_test.py
+++ tests/io_test.py
@@ -1,184 +1,55 @@
 from pathlib import Path
-from unittest.mock import Mock
 
-import numpy as np
 import pytest
-import tensorflow.python.framework.errors_impl as tf_errors
-from astropy.io import fits
-from astropy.table import Table
 
-from gaia.io import FileMode, FileSaver, read, read_fits_table, write
-
-
-TEST_FILEPATH = "./test-dir/test-file.txt"
+from gaia.io import FileSaver
 
 
 @pytest.fixture
-def not_found_error(mocker):
-    """Mock `tf.io.gfile.GFile.__enter__()` to raise a `NotFoundError`."""
-    mocker.patch(
-        "gaia.io.tf.io.gfile.GFile.__enter__",
-        side_effect=tf_errors.NotFoundError("node", "operation", "msg"),
-    )
+def saver(tmp_path):
+    """Return an instance of `FileSaver`."""
+    return FileSaver(tables_dir=tmp_path.as_posix(), time_series_dir=tmp_path.as_posix())
 
 
-@pytest.fixture
-def permission_error(mocker):
-    """Mock `tf.io.gfile.GFile.__enter__()` to raise a `PermissionDeniedError`."""
-    mocker.patch(
-        "gaia.io.tf.io.gfile.GFile.__enter__",
-        side_effect=tf_errors.PermissionDeniedError("node", "operation", "msg"),
-    )
+@pytest.fixture()
+def path_error(mocker):
+    """Factory function to mock `Path.write_bytes()` to raise a specific error."""
+
+    def _mock_error(error):
+        mocker.patch.object(Path, "write_bytes", side_effect=error())
+
+    return _mock_error
 
 
-@pytest.mark.usefixtures("not_found_error")
-@pytest.mark.parametrize("mode", [FileMode.READ, FileMode.READ_BINARY])
-def test_read__file_not_found(mode):
-    """Test that `FileNotFoundError` is raised when a file is not found."""
-    with pytest.raises(FileNotFoundError):
-        read(TEST_FILEPATH, mode)
+@pytest.mark.parametrize("error", [FileNotFoundError, PermissionError])
+def test_save_table__write_error_occured(error, saver, path_error):
+    """Test that correct error is raised when write table error occurred."""
+    path_error(error)
+    with pytest.raises(error):
+        saver.save_table("tab1.csv", b"test data")
 
 
-@pytest.mark.usefixtures("permission_error")
-@pytest.mark.parametrize("mode", [FileMode.READ, FileMode.READ_BINARY])
-def test_read__permission_denied(mode):
-    """Test that `PermissionError` is raised when a user has no permission to read."""
-    with pytest.raises(PermissionError):
-        read(TEST_FILEPATH, mode)
+@pytest.mark.parametrize("error", [FileNotFoundError, PermissionError])
+def test_save_time_series__write_error_occured(error, saver, path_error):
+    """Test that correct error is raised when  write time series error occurred."""
+    path_error(error)
+    with pytest.raises(error):
+        saver.save_time_series("ts1.fits", b"test data")
 
 
-@pytest.mark.parametrize(
-    "mode,expected",
-    [
-        (FileMode.READ, "test"),
-        (FileMode.READ_BINARY, b"test"),
-    ],
-)
-def test_read__read_content(mode, expected, mocker):
-    """Test that the contents of the file are read correctly."""
-    mocker.patch(
-        "gaia.io.tf.io.gfile.GFile.__enter__",
-        return_value=Mock(**{"read.return_value": expected}),
-    )
-    result = read(TEST_FILEPATH, mode)
-    assert result == expected
+def test_save_table__save_data(saver):
+    """Test that a table is correctly saved."""
+    table = b"test data"
+    name = "tab1.csv"
+    saver.save_table(name, table)
+    saved_table = Path(f"{saver._tables_dir}/{name}").read_bytes()
+    assert saved_table == table
 
 
-@pytest.mark.usefixtures("not_found_error")
-@pytest.mark.parametrize("mode,data", [(FileMode.WRITE, "test"), (FileMode.WRITE_BINARY, b"test")])
-def test_write__file_not_found(mode, data):
-    """Test that `FileNotFoundError` is raised when a file is not found."""
-    with pytest.raises(FileNotFoundError):
-        write(TEST_FILEPATH, data, mode)
-
-
-@pytest.mark.usefixtures("permission_error")
-@pytest.mark.parametrize("mode,data", [(FileMode.WRITE, "test"), (FileMode.WRITE_BINARY, b"test")])
-def test_write__permission_denied(mode, data):
-    """Test that `PermissionError` is raised when a user has no permission to write."""
-    with pytest.raises(PermissionError):
-        write(TEST_FILEPATH, data, mode)
-
-
-@pytest.mark.parametrize("mode,data", [(FileMode.WRITE, "test"), (FileMode.WRITE_BINARY, b"test")])
-def test_write__write_content(mode, data, mocker):
-    """Test that the data is written to the file without errors."""
-    mocker.patch("gaia.io.tf.io.gfile.GFile.__enter__", return_value=Mock())
-    write(TEST_FILEPATH, data, mode)
-
-
-class TestFileSaver:
-    @pytest.fixture
-    def local_saver(self, tmp_path):
-        """Return an instance of `FileSaver` configured for a local environment."""
-        return FileSaver(tables_dir=tmp_path.as_posix(), time_series_dir=tmp_path.as_posix())
-
-    @pytest.fixture(params=[FileNotFoundError, PermissionError])
-    def write_error(self, request, mocker):
-        """Mock and return `gaia.io.read()` to raise `FileNotFoundError` and `PermissionError`."""
-        error = request.param
-        mocker.patch("gaia.io.write", side_effect=error)
-        return error
-
-    def test_save_table__write_error_occured(self, write_error, local_saver):
-        """Test that correct error is raised when write table error occurred."""
-        with pytest.raises(write_error):
-            local_saver.save_table("tab1.csv", b"test data")
-
-    def test_save_time_series__write_error_occured(self, write_error, local_saver):
-        """Test that correct error is raised when  write time series error occurred."""
-        with pytest.raises(write_error):
-            local_saver.save_time_series("ts1.fits", b"test data")
-
-    @pytest.fixture
-    def saver(self, request, local_saver):
-        """Return the `FileSaver` instance configured for your local or GCP environment.
-
-        Object configuration is based on the specified data directory path.
-        """
-        path = request.param
-        if path.startswith("gs://"):
-            return FileSaver(
-                tables_dir=f"{path}/tables",
-                time_series_dir=f"{path}/ts",
-            )  # pragma: no cover
-        return local_saver
-
-    @pytest.mark.parametrize(
-        "saver",
-        [
-            "./local/path",
-            pytest.param("gs://gcs/path", marks=pytest.mark.skip(reason="no easy way to test GCS")),
-        ],
-        indirect=True,
-        ids=["local", "GCS"],
-    )
-    def test_save_table__save_data(self, saver):
-        """Test that a table is saved without errors."""
-        table = b"test data"
-        name = "tab1.csv"
-        saver.save_table(name, table)
-        saved_table = Path(f"{saver._tables_dir}/{name}").read_bytes()
-        assert saved_table == table
-
-    @pytest.mark.parametrize(
-        "saver",
-        [
-            "./local/path",
-            pytest.param("gs://gcs/path", marks=pytest.mark.skip(reason="no easy way to test GCS")),
-        ],
-        indirect=True,
-        ids=["local", "GCS"],
-    )
-    def test_save_time_series__save_data(self, saver):
-        """Test that a time series is saved without errors."""
-        series = b"test data"
-        name = "tab1.csv"
-        saver.save_time_series(name, series)
-        saved_series = Path(f"{saver._time_series_dir}/{name}").read_bytes()
-        assert saved_series == series
-
-
-def test_read_fits_table__file_not_found(mocker):
-    """Test that `FileNotFoundError` is raised when no file found."""
-    mocker.patch("gaia.io.read", side_effect=FileNotFoundError())
-    with pytest.raises(FileNotFoundError):
-        read_fits_table(TEST_FILEPATH, "test")
-
-
-def test_read_fits_table__permission_denied(mocker):
-    """Test that `PermissionError` is raised when the user has no permission to read."""
-    mocker.patch("gaia.io.read", side_effect=PermissionError())
-    with pytest.raises(PermissionError):
-        read_fits_table(TEST_FILEPATH, "test")
-
-
-def fits_content(fields, data, hdu_extension):
-    """Create a `astropy.fits.HDUList` which is a representation of FITS file.
-
-    Include columns and data under specifed HDU extension.
-    """
-    hdu1 = fits.PrimaryHDU()
-    table = Table(names=fields, data=np.stack(data, axis=1))
-    hdu2 = fits.BinTableHDU(name=hdu_extension, data=table)
-    return fits.HDUList([hdu1, hdu2])
+def test_save_time_series__save_data(saver):
+    """Test that a time series is correctly saved."""
+    series = b"test data"
+    name = "tab1.csv"
+    saver.save_time_series(name, series)
+    saved_series = Path(f"{saver._time_series_dir}/{name}").read_bytes()
+    assert saved_series == series
