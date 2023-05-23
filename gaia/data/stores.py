df3962694e89ec02689111055f7b1fb044f820d7
Krzysztof Gonet <36476874+Savaed@users.noreply.github.com>
2023-05-23 17:49:18 +0200

    change: change TceRepository to TceStore

--- gaia/data/stores.py
+++ gaia/data/stores.py
@@ -1,10 +1,14 @@
-from typing import Generic, TypedDict, TypeVar
+from collections import Counter
+from functools import cached_property
+from typing import Any, Generic, TypedDict, TypeVar
 
-from gaia.data.mappers import Mapper, MapperError
+from gaia.data.mappers import Mapper, MapperError, map_tce_label
 from gaia.data.models import (
     TCE,
     Id,
+    PeriodicEvent,
     StellarParameters,
+    TceLabel,
     TimeSeries,
 )
 from gaia.io import (
@@ -108,6 +112,17 @@ class StellarParametersStore(Generic[TStellarParameters]):
         Returns:
             TStellarParameters: The stellar physical properties of the target star or system
         """
+        raw_params = self._read(id)
+        mapped_params = self._map(raw_params[0])
+        return mapped_params
+
+    def _map(self, result: dict[str, Any]) -> TStellarParameters:
+        try:
+            return self._mapper(result)
+        except MapperError as ex:
+            raise DataStoreError(ex)
+
+    def _read(self, id: Id) -> list[dict[str, Any]]:
         try:
             result = self._reader.read(where_sql=f"WHERE {self._schema['id']}={id}")
         except MissingColumnError:
@@ -116,12 +131,189 @@ class StellarParametersStore(Generic[TStellarParameters]):
                 "method was not found in the data source",
             )
         except ReaderError as ex:
-            raise DataStoreError(f"Cannot get stellar prameters for {id=}: {ex}")
+            raise DataStoreError(f"Cannot get stellar parameters for {id=}: {ex}")
 
         if not result:
             raise DataNotFoundError(id)
 
+        return result
+
+
+class TceStoreParamsSchema(TypedDict):
+    """`TceStore` methods parameters to data columns names mapping."""
+
+    target_id: str
+    tce_id: str
+    label: str
+    name: str
+    epoch: str
+    duration: str
+    period: str
+
+
+class TceStore(Generic[TTce]):
+    """Threshold-Crossing Events (TCE) datastore, which provides a convenient way to read TCEs."""
+
+    def __init__(
+        self,
+        mapper: Mapper[TTce],
+        reader: TableReader,
+        parameters_schema: TceStoreParamsSchema,
+    ) -> None:
+        self._mapper = mapper
+        self._reader = reader
+        self._schema = parameters_schema
+
+    def get_by_id(self, target_id: Id, tce_id: Id) -> TTce:
+        """Retrieve TCE for the target star or binary/multiple system.
+
+        Args:
+            target_id (Id): Target ID
+            tce_id (Id): TCE ID
+
+        Raises:
+            DataNotFoundError: The requested TCE was not found
+            KeyError: Required key not found in the store parameters schema
+            DataStoreError: Generic error related to mapping or reading the source data
+
+        Returns:
+            TTce: TCE for a specific target and  specified ID
+        """
+        schema = self._schema
+        where = f"WHERE {schema['target_id']}={target_id} AND {schema['tce_id']}={tce_id}"
         try:
-            return self._mapper(result[0])
+            raw_tce = self._reader.read(where_sql=where)
+        except MissingColumnError as ex:
+            raise KeyError(ex.column)
+        except ReaderError as ex:
+            raise DataStoreError(f"Cannot get TCE for {target_id=} and {tce_id=}: {ex}")
+
+        if not raw_tce:
+            raise DataNotFoundError(f"TCE for {target_id=} and {tce_id=} not found")
+
+        mapped_tce = self._map(raw_tce[0])
+        return mapped_tce
+
+    def get_by_name(self, name: str) -> TTce:
+        """Retrieve TCE for a specific name.
+
+        Args:
+            name (str): The name of TCE. For Kepler dataset it should be Kepler Name no KOI Name
+
+        Raises:
+            ValueError: Name is None or an empty string
+            DataNotFoundError: Requested TCE was not found
+            KeyError: Required key not found in the store parameters schema
+            DataStoreError: Generic error related to mapping or reading the source data
+
+        Returns:
+            TTce: TCE with the specifed name
+        """
+        if not name:
+            raise ValueError("Parameter 'name' cannot be None or empty string")
+
+        try:
+            result = self._reader.read(where_sql=f"WHERE {self._schema['name']}='{name.strip()}'")
+        except MissingColumnError as ex:
+            raise KeyError(ex.column)
+        except ReaderError as ex:
+            raise DataStoreError(f"Cannot get TCE for {name=}: {ex}")
+
+        if not result:
+            raise DataNotFoundError(f"TCE for {name=} not found")
+
+        return self._map(result[0])
+
+    def get_all_for_target(self, target_id: Id) -> list[TTce]:
+        """Retrieve all TCEs for the target star or binary/multiple system.
+
+        Args:
+            target_id (Id): Target ID
+
+        Raises:
+            KeyError: Required key not found in the store parameters schema
+            DataStoreError: Generic error related to mapping or reading the source data
+
+        Returns:
+            list[TTce]: All TCEs related to the specified target if any, otherwise an empty list
+        """
+        try:
+            raw_tces = self._reader.read(where_sql=f"WHERE {self._schema['target_id']}={target_id}")
+        except MissingColumnError as ex:
+            raise KeyError(ex.column)
+        except ReaderError as ex:
+            raise DataStoreError(f"Cannot get all TCEs for {target_id=}: {ex}")
+
+        mapped_tces = list(map(self._map, raw_tces))
+        return mapped_tces
+
+    @property
+    def tce_count(self) -> int:
+        """The number of all TCEs in the table."""
+        try:
+            tce_ids = self._reader.read({self._schema["tce_id"]})
+        except MissingColumnError as ex:
+            raise KeyError(ex.column)
+        except ReaderError as ex:
+            raise DataStoreError(f"Cannot get all TCE IDs: {ex}")
+
+        return len(tce_ids)
+
+    @property
+    def unique_target_ids(self) -> list[Id]:
+        """
+        The number of unique IDs of the target star or binaries/multiples in ascending order.
+        """
+        try:
+            target_ids = self._reader.read({self._schema["target_id"]})
+        except MissingColumnError as ex:
+            raise KeyError(ex.column)
+        except ReaderError as ex:
+            raise DataStoreError(f"Cannot get all target IDs: {ex}")
+
+        unique_target_ids = {list(target_id.values())[0] for target_id in target_ids}
+        return sorted(unique_target_ids)
+
+    @cached_property
+    def events(self) -> list[tuple[Id, Id, PeriodicEvent]]:
+        """Transits for all TCEs in form of `(target_id, tce_id, event)`."""
+        try:
+            raw_tces = self._reader.read()
+        except MissingColumnError as ex:
+            raise KeyError(ex.column)
+        except ReaderError as ex:
+            raise DataStoreError(f"Cannot get all TCEs: {ex}")
+
+        mapped_tces = map(self._map, raw_tces)
+        return [(tce.target_id, tce.id, tce.event) for tce in mapped_tces]
+
+    @property
+    def labels_distribution(self) -> dict[TceLabel, int]:
+        """Distribution of TCE labels. Ambiguous labels are assumed to be `TceLabel.UNKNOWN`."""
+        distribution = dict.fromkeys(TceLabel, 0)  # Make sure all labels are present
+
+        try:
+            results = self._reader.read([self._schema["label"]])
+        except MissingColumnError as ex:
+            raise KeyError(ex.column)
+        except ReaderError as ex:
+            raise DataStoreError(f"Cannot get all TCE labels: {ex}")
+
+        counts = Counter(map(self._map_tce_label, results))
+        distribution |= dict(counts)
+
+        if not distribution[TceLabel.FP]:
+            distribution[TceLabel.FP] = distribution[TceLabel.NTP] + distribution[TceLabel.AFP]
+
+        return distribution
+
+    def _map_tce_label(self, label: dict[str, str]) -> TceLabel:
+        return map_tce_label(list(label.values())[0])
+
+    def _map(self, result: dict[str, Any]) -> TTce:
+        try:
+            mapped_tce = self._mapper(result)
         except MapperError as ex:
             raise DataStoreError(ex)
+
+        return mapped_tce
--- tests/data/stores_test.py
+++ tests/data/stores_test.py
@@ -4,12 +4,14 @@ import numpy as np
 import pytest
 
 from gaia.data.mappers import MapperError
-from gaia.data.models import StellarParameters, TimeSeries
+from gaia.data.models import TCE, PeriodicEvent, StellarParameters, TceLabel, TimeSeries
 from gaia.data.stores import (
     DataStoreError,
     MissingPeriodsError,
     StellarParametersStore,
     StellarParametersStoreParamsSchema,
+    TceStore,
+    TceStoreParamsSchema,
     TimeSeriesStore,
 )
 from gaia.io import ByIdReader, DataNotFoundError, MissingColumnError, ReaderError, TableReader
@@ -24,7 +26,10 @@ def time_series_reader():
 
 @pytest.fixture
 def mapper():
-    """Return a `Mapper` callable that returns the unchanged passed object."""
+    """
+    Return a `Mapper` callable that takes a songle argument of any type and returns the unchanged
+    passed object.
+    """
     return lambda source: source
 
 
@@ -64,14 +69,26 @@ TEST_TIME_SERIES = [
 ]
 
 
-def test_time_series_store_get__map_error(time_series_reader, create_time_series_store):
-    """Test that `DataStoreError` is raised when generic map error occured."""
-    time_series_reader.read_by_id.return_value = TEST_TIME_SERIES
+@pytest.fixture
+def erroneous_mapper():
+    """
+    Return a `Mapper` callable that takes a single argument of any type and raises `MapperError`.
+    """
 
     def mapper(_):
         raise MapperError
 
-    store = create_time_series_store(mapper, time_series_reader)
+    return mapper
+
+
+def test_time_series_store_get__map_error(
+    time_series_reader,
+    create_time_series_store,
+    erroneous_mapper,
+):
+    """Test that `DataStoreError` is raised when generic map error occured."""
+    time_series_reader.read_by_id.return_value = TEST_TIME_SERIES
+    store = create_time_series_store(erroneous_mapper, time_series_reader)
     with pytest.raises(DataStoreError):
         store.get(1)
 
@@ -123,7 +140,7 @@ def table_reader():
 
 @pytest.fixture
 def create_stellar_store():
-    """Factory function to create `return StellarParametersStore[StellarParameters]` object."""
+    """Factory function to create `StellarParametersStore[StellarParameters]` object."""
 
     def create(mapper, reader, schema=None):
         return StellarParametersStore[StellarParameters](
@@ -157,7 +174,7 @@ def test_stellar_parameters_store_get__parameters_schema_error(
     create_stellar_store,
 ):
     """Test that `KeyError` is raised when parameters schema is invalid (e.g. column is missing)."""
-    table_reader.read.side_effect = MissingColumnError
+    table_reader.read.side_effect = MissingColumnError("column")
     store = create_stellar_store(mapper, table_reader)
     with pytest.raises(KeyError):
         store.get(1)
@@ -166,14 +183,14 @@ def test_stellar_parameters_store_get__parameters_schema_error(
 TEST_STELLAR_PARAMETERS = [{"id": 1}]
 
 
-def test_stellar_parameters_store_get__map_error(table_reader, create_stellar_store):
+def test_stellar_parameters_store_get__map_error(
+    table_reader,
+    create_stellar_store,
+    erroneous_mapper,
+):
     """Test that `DataStoreError` is raised when map error occured."""
     table_reader.read.return_value = TEST_STELLAR_PARAMETERS
-
-    def mapper(_):
-        raise MapperError
-
-    store = create_stellar_store(mapper, table_reader)
+    store = create_stellar_store(erroneous_mapper, table_reader)
     with pytest.raises(DataStoreError):
         store.get(1)
 
@@ -188,3 +205,366 @@ def test_stellar_parameters_store_get__return_correct_data(
     store = create_stellar_store(mapper, table_reader)
     actual = store.get(1)
     assert actual == TEST_STELLAR_PARAMETERS[0]
+
+
+@pytest.fixture
+def create_tce_store():
+    """Factory function to create `TceStore[TCE]` object."""
+
+    def create(mapper, reader, schema=None):
+        return TceStore[TCE](
+            mapper=mapper,
+            reader=reader,
+            parameters_schema=schema
+            or TceStoreParamsSchema(
+                target_id="target_id",
+                tce_id="tce_id",
+                label="label",
+                duration="duration",
+                epoch="epoch",
+                name="name",
+                period="period",
+            ),
+        )
+
+    return create
+
+
+def test_tce_store_get_by_id__read_error(mapper, table_reader, create_tce_store):
+    """Test that `DataStoreError` is raised when generic read error occured."""
+    table_reader.read.side_effect = ReaderError
+    store = create_tce_store(mapper, table_reader)
+
+    with pytest.raises(DataStoreError):
+        store.get_by_id(1, 1)
+
+
+def test_tce_store_get_by_id__data_not_found(mapper, table_reader, create_tce_store):
+    """Test that `DataNotFoundError` is raised when no requested TCE was found."""
+    table_reader.read.return_value = []
+    store = create_tce_store(mapper, table_reader)
+    with pytest.raises(DataNotFoundError):
+        store.get_by_id(1, 1)
+
+
+def test_tce_store_get_by_id__parameters_schema_error(mapper, table_reader, create_tce_store):
+    """Test that `KeyError` is raised when parameters schema is invalid (e.g. column is missing)."""
+    table_reader.read.side_effect = MissingColumnError("column")
+    store = create_tce_store(mapper, table_reader)
+    with pytest.raises(KeyError):
+        store.get_by_id(1, 1)
+
+
+TEST_TCE = [
+    dict(tce_id=1, target_id=1, epoch=1.2, duration=2.3, period=3.4, name="tce1_1", label="PC"),
+]
+
+
+def test_tce_store_get_by_id__map_error(table_reader, create_tce_store, erroneous_mapper):
+    """Test that `DataStoreError` is raised when map error occured."""
+    table_reader.read.return_value = TEST_TCE
+    store = create_tce_store(erroneous_mapper, table_reader)
+    with pytest.raises(DataStoreError):
+        store.get_by_id(1, 1)
+
+
+def test_tce_store_get_by_id__return_correct_data(mapper, table_reader, create_tce_store):
+    """Test that correct TCE is returned."""
+    table_reader.read.return_value = TEST_TCE
+    store = create_tce_store(mapper, table_reader)
+    actual = store.get_by_id(1, 1)
+    assert actual == TEST_TCE[0]
+
+
+def test_tce_store_get_by_name__read_error(mapper, table_reader, create_tce_store):
+    """Test that `DataStoreError` is raised when generic read error occured."""
+    table_reader.read.side_effect = ReaderError
+    store = create_tce_store(mapper, table_reader)
+    with pytest.raises(DataStoreError):
+        store.get_by_name("tce1_1")
+
+
+def test_tce_store_get_by_name__data_not_found(mapper, table_reader, create_tce_store):
+    """Test that `DataNotFoundError` is raised when no requested TCE was found."""
+    table_reader.read.return_value = []
+    store = create_tce_store(mapper, table_reader)
+    with pytest.raises(DataNotFoundError):
+        store.get_by_name("tce1_1")
+
+
+def test_tce_store_get_by_name__parameters_schema_error(mapper, table_reader, create_tce_store):
+    """Test that `KeyError` is raised when parameters schema is invalid (e.g. column is missing)."""
+    table_reader.read.side_effect = MissingColumnError("column")
+    store = create_tce_store(mapper, table_reader)
+    with pytest.raises(KeyError):
+        store.get_by_name("tce1_1")
+
+
+def test_tce_store_get_by_name__map_error(table_reader, create_tce_store, erroneous_mapper):
+    """Test that `DataStoreError` is raised when map error occured."""
+    table_reader.read.return_value = TEST_TCE
+    store = create_tce_store(erroneous_mapper, table_reader)
+    with pytest.raises(DataStoreError):
+        store.get_by_name("tce1_1")
+
+
+@pytest.mark.parametrize("name", [None, ""])
+def test_tce_store_get_by_name__name_none_or_empty(name, mapper, table_reader, create_tce_store):
+    """Test that correct TCE is returned."""
+    store = create_tce_store(mapper, table_reader)
+    with pytest.raises(ValueError):
+        store.get_by_name(name)
+
+
+def test_tce_store_get_by_name__return_correct_data(mapper, table_reader, create_tce_store):
+    """Test that correct TCE is returned."""
+    table_reader.read.return_value = TEST_TCE
+    store = create_tce_store(mapper, table_reader)
+    actual = store.get_by_name("tce1_1")
+    assert actual == TEST_TCE[0]
+
+
+def test_tce_store_get_all_for_target__read_error(mapper, table_reader, create_tce_store):
+    """Test that `DataStoreError` is raised when generic read error occured."""
+    table_reader.read.side_effect = ReaderError
+    store = create_tce_store(mapper, table_reader)
+    with pytest.raises(DataStoreError):
+        store.get_all_for_target(1)
+
+
+def test_tce_store_get_all_for_target__parameters_schema_error(
+    mapper,
+    table_reader,
+    create_tce_store,
+):
+    """Test that `KeyError` is raised when parameters schema is invalid (e.g. column is missing)."""
+    table_reader.read.side_effect = MissingColumnError("column")
+    store = create_tce_store(mapper, table_reader)
+    with pytest.raises(KeyError):
+        store.get_all_for_target(1)
+
+
+def test_tce_store_get_all_for_target__map_error(table_reader, create_tce_store, erroneous_mapper):
+    """Test that `DataStoreError` is raised when map error occured."""
+    table_reader.read.return_value = TEST_TCE
+    store = create_tce_store(erroneous_mapper, table_reader)
+    with pytest.raises(DataStoreError):
+        store.get_all_for_target(1)
+
+
+@pytest.mark.parametrize(
+    "read_tces,expected",
+    [([], []), [TEST_TCE, TEST_TCE]],
+    ids=["empty_list", "tces"],
+)
+def test_tce_store_get_all_for_target__return_correct_data(
+    read_tces,
+    expected,
+    mapper,
+    table_reader,
+    create_tce_store,
+):
+    """Test that correct TCEs is returned."""
+    table_reader.read.return_value = read_tces
+    store = create_tce_store(mapper, table_reader)
+    actual = store.get_all_for_target(1)
+    assert actual == expected
+
+
+def test_tce_store_tce_count__read_error(mapper, table_reader, create_tce_store):
+    """Test that `DataStoreError` is raised when generic read error occured."""
+    table_reader.read.side_effect = ReaderError
+    store = create_tce_store(mapper, table_reader)
+    with pytest.raises(DataStoreError):
+        store.tce_count
+
+
+def test_tce_store_tce_count__parameters_schema_error(mapper, table_reader, create_tce_store):
+    """Test that `KeyError` is raised when parameters schema is invalid (e.g. column is missing)."""
+    table_reader.read.side_effect = MissingColumnError("columns")
+    store = create_tce_store(mapper, table_reader)
+    with pytest.raises(KeyError):
+        store.tce_count
+
+
+@pytest.mark.parametrize(
+    "read_tce_ids,expected",
+    [
+        ([], 0),
+        ([dict(target_id=1), dict(target_id=2)], 2),
+        ([dict(target_id=1), dict(target_id=1)], 2),
+        ([dict(target_id=1), dict(target_id=1), dict(target_id=2)], 3),
+    ],
+    ids=["empty_list", "all_unique", "all_duplicates", "some_duplicates"],
+)
+def test_tce_store_tce_count__return_correct_data(
+    read_tce_ids,
+    expected,
+    mapper,
+    table_reader,
+    create_tce_store,
+):
+    table_reader.read.return_value = read_tce_ids
+    store = create_tce_store(mapper, table_reader)
+    actual = store.tce_count
+    assert actual == expected
+
+
+def test_tce_store_unique_target_ids__read_error(mapper, table_reader, create_tce_store):
+    """Test that `DataStoreError` is raised when generic read error occured."""
+    table_reader.read.side_effect = ReaderError
+    store = create_tce_store(mapper, table_reader)
+    with pytest.raises(DataStoreError):
+        store.unique_target_ids
+
+
+def test_tce_store_unique_target_ids__parameters_schema_error(
+    mapper,
+    table_reader,
+    create_tce_store,
+):
+    """Test that `KeyError` is raised when parameters schema is invalid (e.g. column is missing)."""
+    table_reader.read.side_effect = MissingColumnError("columns")
+    store = create_tce_store(mapper, table_reader)
+    with pytest.raises(KeyError):
+        store.unique_target_ids
+
+
+@pytest.mark.parametrize(
+    "read_tce_ids,expected",
+    [
+        ([], []),
+        ([dict(target_id=1), dict(target_id=2)], [1, 2]),
+        ([dict(target_id=1), dict(target_id=1)], [1]),
+        ([dict(target_id=1), dict(target_id=1), dict(target_id=2)], [1, 2]),
+    ],
+    ids=["empty_list", "all_unique", "all_duplicates", "some_duplicates"],
+)
+def test_tce_store_unique_target_ids__return_correct_data(
+    read_tce_ids,
+    expected,
+    mapper,
+    table_reader,
+    create_tce_store,
+):
+    table_reader.read.return_value = read_tce_ids
+    store = create_tce_store(mapper, table_reader)
+    actual = store.unique_target_ids
+    assert actual == expected
+
+
+def test_tce_store_events__read_error(mapper, table_reader, create_tce_store):
+    """Test that `DataStoreError` is raised when generic read error occured."""
+    table_reader.read.side_effect = ReaderError
+    store = create_tce_store(mapper, table_reader)
+    with pytest.raises(DataStoreError):
+        store.events
+
+
+def test_tce_store_events__parameters_schema_error(mapper, table_reader, create_tce_store):
+    """Test that `KeyError` is raised when parameters schema is invalid (e.g. column is missing)."""
+    table_reader.read.side_effect = MissingColumnError("columns")
+    store = create_tce_store(mapper, table_reader)
+    with pytest.raises(KeyError):
+        store.events
+
+
+@pytest.fixture
+def tce_event_mapper():
+    def mapper(tce):
+        return TCE(
+            name=tce["name"],
+            id=tce["tce_id"],
+            target_id=tce["target_id"],
+            label=TceLabel.PC,
+            event=PeriodicEvent(tce["epoch"], tce["duration"], tce["period"]),
+        )
+
+    return mapper
+
+
+@pytest.mark.parametrize(
+    "read_tce_ids,expected",
+    [([], []), (TEST_TCE, [(1, 1, PeriodicEvent(epoch=1.2, duration=2.3, period=3.4))])],
+    ids=["empty_list", "tces"],
+)
+def test_tce_store_events__return_correct_data(
+    read_tce_ids,
+    expected,
+    tce_event_mapper,
+    table_reader,
+    create_tce_store,
+):
+    table_reader.read.return_value = read_tce_ids
+    store = create_tce_store(tce_event_mapper, table_reader)
+    actual = store.events
+    assert actual == expected
+
+
+def test_tce_store_labels_distribution__read_error(mapper, table_reader, create_tce_store):
+    """Test that `DataStoreError` is raised when generic read error occured."""
+    table_reader.read.side_effect = ReaderError
+    store = create_tce_store(mapper, table_reader)
+    with pytest.raises(DataStoreError):
+        store.labels_distribution
+
+
+def test_tce_store_labels_distribution__parameters_schema_error(
+    mapper,
+    table_reader,
+    create_tce_store,
+):
+    """Test that `KeyError` is raised when parameters schema is invalid (e.g. column is missing)."""
+    table_reader.read.side_effect = MissingColumnError("columns")
+    store = create_tce_store(mapper, table_reader)
+    with pytest.raises(KeyError):
+        store.labels_distribution
+
+
+@pytest.mark.parametrize(
+    "read_labels,expected",
+    [
+        (
+            [],
+            {TceLabel.PC: 0, TceLabel.AFP: 0, TceLabel.NTP: 0, TceLabel.FP: 0, TceLabel.UNKNOWN: 0},
+        ),
+        (
+            [dict(label="PC")],
+            {TceLabel.PC: 1, TceLabel.AFP: 0, TceLabel.NTP: 0, TceLabel.FP: 0, TceLabel.UNKNOWN: 0},
+        ),
+        (
+            [
+                dict(label="PC"),
+                dict(label="AFP"),
+                dict(label="NTP"),
+                dict(label="FP"),
+                dict(label="UNKNOWN"),
+            ],
+            {TceLabel.PC: 1, TceLabel.AFP: 1, TceLabel.NTP: 1, TceLabel.FP: 1, TceLabel.UNKNOWN: 1},
+        ),
+        (
+            [dict(label="AFP"), dict(label="NTP")],
+            {TceLabel.PC: 0, TceLabel.AFP: 1, TceLabel.NTP: 1, TceLabel.FP: 2, TceLabel.UNKNOWN: 0},
+        ),
+        (
+            [dict(label="PC"), dict(label="PC")],
+            {TceLabel.PC: 2, TceLabel.AFP: 0, TceLabel.NTP: 0, TceLabel.FP: 0, TceLabel.UNKNOWN: 0},
+        ),
+        (
+            [dict(label="test"), dict(label="UNKNOWN")],
+            {TceLabel.PC: 0, TceLabel.AFP: 0, TceLabel.NTP: 0, TceLabel.FP: 0, TceLabel.UNKNOWN: 2},
+        ),
+    ],
+)
+def test_tce_store_labels_distribution__return_correct_data(
+    read_labels,
+    expected,
+    mapper,
+    table_reader,
+    create_tce_store,
+):
+    """Test that correct TCE labels distribution is returned and FP is computed if required."""
+    table_reader.read.return_value = read_labels
+    store = create_tce_store(mapper, table_reader)
+    actual = store.labels_distribution
+    assert actual == expected
