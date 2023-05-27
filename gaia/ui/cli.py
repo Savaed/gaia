898b3695f0d04ce7ef7704bd1571388284197596
Krzysztof Gonet <36476874+Savaed@users.noreply.github.com>
2023-05-27 08:32:12 +0200

    feat: add print_header() util function for cli

--- gaia/ui/cli.py
+++ gaia/ui/cli.py
@@ -1,5 +1,7 @@
-from typing import Any
+from pathlib import Path
 
+import hydra
+import tomli
 from rich.console import Console
 
 
@@ -13,17 +15,38 @@ GAIA_HEADER = """
 """
 
 
-def print_header(description: str | None = None, **kwargs: Any) -> None:
+def print_header(description: str | None = None, hydra_info: bool = True) -> None:
+    """Display project main information (project description and version from 'pyproject.toml').
+
+    Args:
+        description (str | None, optional): Detailed description. Defaults to None.
+        hydra_info (bool, optional): Whether to print a config path. Hydra config must be set.
+            Defaults to True.
+    """
+    project_root_dir = Path(__file__).parents[2]
+    pyproject = tomli.loads((project_root_dir / "pyproject.toml").read_text())
+    project_section = pyproject["project"]
+    project_version = project_section["version"]
+    project_description = project_section["description"]
+
     with Console(width=120) as console:
         console.print(f"[green]{GAIA_HEADER}")
+        console.print(f"v. {project_version}\n")
+        console.print(f"🌍 [bold]{project_description}[/bold]\n")
 
         if description:
-            console.print(f"[bold]{description}\n")
+            console.print(description)
 
-        if kwargs:
-            max_key_len = max(map(len, kwargs))
-
-            for key, value in kwargs.items():
-                console.print(f"  [dim]{key:.<{max_key_len+3}} {value}")
+        if hydra_info:
+            try:
+                hydra_config_path = Path(
+                    hydra.core.hydra_config.HydraConfig.get()["runtime"]["output_dir"],
+                )
+            except ValueError:
+                console.print("❌ Hydra configuration is not set")
+            else:
+                console.print(
+                    f"📜 Script configuration can be found at: {hydra_config_path / '.hydra'/ 'config.yaml'}",  # 'config.yaml' by convention, but may change # noqa
+                )
 
         console.rule()
