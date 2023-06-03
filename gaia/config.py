6da56dbabe30f3889b8e17c9f4ad49d6a5f5b041
Krzysztof Gonet <36476874+Savaed@users.noreply.github.com>
2023-06-03 14:44:46 +0200

    change: redesign application configuration

--- gaia/config.py
+++ gaia/config.py
@@ -1,78 +1,66 @@
+from dataclasses import dataclass
 from pathlib import Path
 from typing import Any
 
-from pydantic import BaseModel, DirectoryPath, FilePath, HttpUrl, PositiveInt
+from pydantic import BaseModel, DirectoryPath, HttpUrl, PositiveInt, validator
 
-from gaia.enums import Cadence
+from gaia.downloaders import HTTPFileRequest
+from gaia.enums import Cadence, Mission
+from gaia.io import create_dir_if_not_exist
 
 
-class DataApiConfig(BaseModel):
-    mast_base_url: HttpUrl
+# NOTE: @dataclass when '_target_' or '_partial_' is present because Pydantic excludes these fields
+# See: https://docs.pydantic.dev/latest/usage/models/#automatically-excluded-attributes
+
+
+@dataclass
+class SaverConfig:
+    _target_: str  # '_target_' is repeated because of some weird behaviour of Pydantic + dataclass
+    tables_dir: str
+    time_series_dir: str
+
+
+@dataclass
+class DataDownloaderConfig:
+    _target_: str
+    saver: SaverConfig
+    cadence: Cadence
     nasa_base_url: HttpUrl
+    mast_base_url: HttpUrl
     num_async_requests: PositiveInt
-    requests: list[tuple[HttpUrl, Path]]
+
+    class Config:
+        arbitrary_types_allowed = True
 
 
-class DataLoadConfig(BaseModel):
-    skip_tables: bool
-    observation_cadence: Cadence
-    tce_file: Path
-    target_id_column: str
-    api: DataApiConfig
+class DataDownloadConfig(BaseModel):
     verbose: bool
+    download_tables: bool
+    download_time_series: bool
+    downloader: DataDownloaderConfig
+    tables_requests: list[HTTPFileRequest]
+    script_description: str
+    tce_file_path: Path
+    tce_file_target_id_column: str
+
+    class Config:
+        arbitrary_types_allowed = True
 
 
-class TceMergeConfig(BaseModel):
-    select_sql: str
-    join_sql: str
-    label_conditions_case_sql: list[str]
-    output_file: Path
+class AppConfig(BaseModel):
+    mission: Mission
+    raw_tables_dir: DirectoryPath
+    raw_time_series_dir: DirectoryPath
+    interim_tables_dir: DirectoryPath
+    interim_time_series_dir: DirectoryPath
+    download: DataDownloadConfig | None = None
 
-
-class FileConvertConfig(BaseModel):
-    inputs: FilePath | str
-    output: Path
-    columns: list[str]
-    columns_map: dict[str, str]
-
-
-class TimeSeriesConvertConfig(BaseModel):
-    inputs: FilePath | str
-    output: Path
-    data_header: str
-    data_columns: list[str]
-    meta_columns: list[str]
-    names_map: dict[str, str]
-    path_target_id_pattern: str
-
-
-class DataConvertConfig(BaseModel):
-    tce: FileConvertConfig
-    stellar: FileConvertConfig
-    time_series: TimeSeriesConvertConfig
-
-
-class DataConfig(BaseModel):
-    raw_tables_dir: Path
-    raw_time_series_dir: Path
-    interim_tables_dir: Path
-    interim_time_series_dir: Path
-    load: DataLoadConfig
-    convert: DataConvertConfig
-    tce_merge: TceMergeConfig
-
-
-class UiConfig(BaseModel):
-    assets_dir: DirectoryPath
-    external_stylesheets: list[HttpUrl]
-    available_graphs: dict[str, str]
-    tce_filepath: FilePath
-    stellar_parameters_filepath: FilePath
-    time_series_dir: DirectoryPath
-    server_params: dict[str, Any]
-
-
-class Config(BaseModel):
-    mission: str
-    data: DataConfig
-    ui: UiConfig
+    @validator(
+        "raw_tables_dir",
+        "raw_time_series_dir",
+        "interim_tables_dir",
+        "interim_time_series_dir",
+        pre=True,
+    )
+    def create_dir_if_not_exist(cls, path_like: Any) -> Path:
+        return create_dir_if_not_exist(path_like)
