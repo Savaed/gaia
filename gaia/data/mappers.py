cb0472b9f7318b0b681b213af64cb96d698d4bea
Krzysztof Gonet <36476874+Savaed@users.noreply.github.com>
2023-07-10 12:31:50 +0200

    feat: add map_kepler_tce()

--- gaia/data/mappers.py
+++ gaia/data/mappers.py
@@ -4,9 +4,13 @@ import numpy as np
 
 from gaia.data.models import (
     KeplerStellarParameters,
+    KeplerTCE,
     KeplerTimeSeries,
+    PeriodicEvent,
     RawKeplerStellarParameter,
+    RawKeplerTce,
     RawKeplerTimeSeries,
+    TceLabel,
 )
 
 
@@ -46,3 +50,37 @@ def map_kepler_stallar_parameters(source: RawKeplerStellarParameter) -> KeplerSt
         )
     except KeyError as ex:
         raise MapperError(f"Key '{ex}' not found in the source RawKeplerStellarParameter object")
+
+
+def map_tce_label(label: str) -> TceLabel:
+    try:
+        return TceLabel[label]
+    except KeyError:
+        try:
+            return TceLabel(label)
+        except ValueError:
+            return TceLabel.UNKNOWN
+
+
+def map_kepler_tce(source: RawKeplerTce) -> KeplerTCE:
+    try:
+        return KeplerTCE(
+            id=source["tce_plnt_num"],
+            target_id=source["kepid"],
+            name=source["kepler_name"],
+            label=map_tce_label(source["label"]),
+            event=PeriodicEvent(
+                epoch=source["tce_time0bk"],
+                duration=source["tce_duration"] / 24,  # Convert hours to days
+                period=source["tce_period"],
+            ),
+            opt_ghost_core_aperture_corr=source["tce_cap_stat"],
+            opt_ghost_halo_aperture_corr=source["tce_hap_stat"],
+            bootstrap_false_alarm_proba=source["boot_fap"],
+            fitted_period=source["tcet_period"],
+            radius=source["tce_prad"],
+            rolling_band_fgt=source["tce_rb_tcount0"],
+            transit_depth=source["tce_depth"],
+        )
+    except KeyError as ex:
+        raise MapperError(f"Key '{ex}' not found in the source RawKeplerTce object")
--- tests/mappers_test.py
+++ tests/mappers_test.py
@@ -1,12 +1,22 @@
 import numpy as np
 import pytest
 
-from gaia.data.mappers import MapperError, map_kepler_stallar_parameters, map_kepler_time_series
+from gaia.data.mappers import (
+    MapperError,
+    map_kepler_stallar_parameters,
+    map_kepler_tce,
+    map_kepler_time_series,
+    map_tce_label,
+)
 from gaia.data.models import (
     KeplerStellarParameters,
+    KeplerTCE,
     KeplerTimeSeries,
+    PeriodicEvent,
     RawKeplerStellarParameter,
+    RawKeplerTce,
     RawKeplerTimeSeries,
+    TceLabel,
 )
 from tests.conftest import assert_dict_with_numpy_equal
 
@@ -47,7 +57,7 @@ def test_map_kepler_time_series__missing_key_in_source_data():
 
 
 def test_map_kepler_stallar_parameters__data_mapped_correctly():
-    """Test that `RawKeplerTimeSeries` is correctly mapped to `KeplerTimeSeries`."""
+    """Test that `RawKeplerStellarParameter` is correctly mapped to `KeplerStellarParameters`."""
     raw_stellar_params = RawKeplerStellarParameter(
         kepid=1,
         teff=2.0,
@@ -78,7 +88,86 @@ def test_map_kepler_stallar_parameters__missing_key_in_source_data():
         dens=3.0,
         logg=4.0,
         feh=5.0,
-        radius=6.0,  # Missing `mass`
+        radius=6.0,  # Missing `mass`.
     )
     with pytest.raises(MapperError):
         map_kepler_stallar_parameters(raw_stellar_params)  # type: ignore
+
+
+@pytest.mark.parametrize(
+    "label,expected",
+    [
+        ("PC", TceLabel.PC),
+        ("PLANET_CANDIDATE", TceLabel.PC),
+        ("FP", TceLabel.FP),
+        ("FALSE_POSITIVE", TceLabel.FP),
+        ("AFP", TceLabel.AFP),
+        ("ASTROPHYSICAL_FALSE_POSITIVE", TceLabel.AFP),
+        ("NTP", TceLabel.NTP),
+        ("NON_TRANSITING_PHENOMENA", TceLabel.NTP),
+        ("UNKNOWN", TceLabel.UNKNOWN),
+        ("TEST", TceLabel.UNKNOWN),
+    ],
+)
+def test_map_tce_label__label_mapped_correctly(label, expected):
+    """Test that TCE label is mapped correctly."""
+    actual = map_tce_label(label)
+    assert actual == expected
+
+
+@pytest.mark.parametrize("kepler_name", ["kepler_name", None])
+def test_map_kepler_tce__data_mapped_correctly(kepler_name):
+    """Test that `RawKeplerStellarParameter` is correctly mapped to `KeplerStellarParameters`."""
+    raw_tce = RawKeplerTce(
+        kepid=1,
+        tce_plnt_num=2,
+        tce_cap_stat=2.0,
+        tce_hap_stat=3.0,
+        boot_fap=4.0,
+        tce_rb_tcount0=5.0,
+        tce_prad=6.0,
+        tcet_period=7.0,
+        tce_depth=8.0,
+        tce_time0bk=9.0,
+        tce_duration=24.0,
+        tce_period=10.0,
+        kepler_name=kepler_name,
+        label=TceLabel.PC.name,  # Tce label mapping tested separately.
+    )
+    expected = KeplerTCE(
+        id=2,
+        target_id=1,
+        name=kepler_name,
+        label=TceLabel.PC,
+        event=PeriodicEvent(duration=1.0, epoch=9.0, period=10.0),
+        opt_ghost_core_aperture_corr=2.0,
+        opt_ghost_halo_aperture_corr=3.0,
+        bootstrap_false_alarm_proba=4.0,
+        rolling_band_fgt=5.0,
+        radius=6.0,
+        fitted_period=7.0,
+        transit_depth=8.0,
+    )
+    actual = map_kepler_tce(raw_tce)
+    assert actual == expected
+
+
+def test_map_kepler_tce__missing_key_in_source_data():
+    """Test that `MapperError` is raised when the source dict has no required key(s)."""
+    raw_tce = dict(
+        kepid=1,
+        tce_cap_stat=2.0,
+        tce_hap_stat=3.0,
+        boot_fap=4.0,
+        tce_rb_tcount0=5.0,
+        tce_prad=6.0,
+        tcet_period=7.0,
+        tce_depth=8.0,
+        tce_time0bk=9.0,
+        tce_duration=24.0,
+        tce_period=10.0,
+        kepler_name="kepler_name",
+        label=TceLabel.PC.name,
+    )  # Missing `tce_plnt_num`.
+    with pytest.raises(MapperError):
+        map_kepler_tce(raw_tce)  # type: ignore
